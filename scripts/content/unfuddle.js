6285d5ca4e70d6e5caf5d52f9823096a8a881fa6
Sepehr Lajevardi <me@sepehr.ws>
2013-08-09 11:25:17 +0430

    Bind the behavior on Hashchange event.

--- scripts/content/unfuddle.js
+++ scripts/content/unfuddle.js
@@ -19,13 +19,13 @@
    * @return {string}
    */
   function createTimerElem(ticketDesc) {
-    var wrapperElem = createTag('span', 'toggl'),
-        timerElem   = createLink('toggl-button unfuddle', 'span');
+    var wrapperElem = createTag("span", "toggl"),
+        timerElem   = createLink("toggl-button unfuddle", "span");
 
     // Append separator:
-    wrapperElem.appendChild(createTag('span', 'separator', '|'));
+    wrapperElem.appendChild(createTag("span", "separator", "|"));
     // Append icon:
-    wrapperElem.appendChild(createTag('span', 'icon icon-time'));
+    wrapperElem.appendChild(createTag("span", "icon icon-time"));
     // Append timer:
     wrapperElem.appendChild(timerElem);
 
@@ -37,12 +37,12 @@
     // Bind a click behavior:
     timerElem.addEventListener("click", function (e) {
       chrome.extension.sendMessage({
-        type:        'timeEntry',
+        type:        "timeEntry",
         description: ticketDesc
       });
 
       // When started, link to toggl app:
-      timerElem.innerHTML = '<a href="http://toggl.com/" style="color:#333" target="_blank">Timer started...</a>';
+      timerElem.innerHTML = "<a href="http://toggl.com/" style="color:#333" target="_blank">Timer started...</a>";
       return false;
     });
 
@@ -58,7 +58,7 @@
    */
   function addLinkToTicket() {
     // Wait for the page to get completely loaded:
-    if ($('.ticket-header') === null) {
+    if ($(".ticket-header") === null) {
       window.setTimeout(function() {
         addLinkToTicket();
       }, 300);
@@ -68,20 +68,31 @@
 
     // Get ticket title & number:
     var timerElem,
-        ticketNum  = $('.ticket-header h1.summary .number').innerText,
-        ticketDesc = $('.ticket-header h1.summary .text-field-text').innerText;
+        ticketNum  = $(".ticket-header h1.summary .number").innerText,
+        ticketDesc = $(".ticket-header h1.summary .text-field-text").innerText;
 
     // Create timer element & append it to the ticket header:
-    timerElem = createTimerElem(ticketNum + ': ' + ticketDesc);
+    timerElem = createTimerElem(ticketNum + ": " + ticketDesc);
     $(".primary-properties").appendChild(timerElem);
   }
 
   // ------------------------------------------------------------------------
 
-  chrome.extension.sendMessage({type: 'activate'}, function (response) {
-    // Inject only if on ticket page:
-    if (response.success && location.hash.indexOf('/by_number/') != -1) {
-      addLinkToTicket();
+  chrome.extension.sendMessage({type: "activate"}, function (response) {
+    if (response.success) {
+
+      // Initial setup, if currently on a ticket page:
+      if (location.hash.indexOf("/by_number/") != -1) {
+        addLinkToTicket();
+      }
+
+      // Bind a behavior on URI hash change & check if we"re ended on a ticket page:
+      window.addEventListener("hashchange", function(e) {
+        if (location.hash.indexOf("/by_number/") != -1) {
+          addLinkToTicket();
+        }
+      }, false);
+
     }
   });
 

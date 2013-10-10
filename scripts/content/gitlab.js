54bd47b0af412a21e8982661f3091f01ac933e79
Ricardo Sateler <rsateler@gmail.com>
2013-10-10 12:08:45 -0300

    Added Gitlab integration

---
+++ scripts/content/gitlab.js
@@ -0,0 +1,40 @@
+/*jslint indent: 2 */
+/*global window: false, document: false, chrome: false, $: false, createTag: false, createLink: false*/
+(function () {
+  "use strict";
+  function createTimerLink(task, moreClass) {
+    var link = createLink('toggl-button gitlab ' + moreClass, 'span');
+    link.addEventListener("click", function (e) {
+      chrome.extension.sendMessage({
+        type: 'timeEntry',
+        description: task
+      });
+      link.innerHTML = "Started...";
+      return false;
+    });
+    return link;
+  }
+
+  function addLinkToDiscussion() {
+    var titleElem = $('body.project .ui-box .ui-box-head .box-title'), numElem, title, buttonGroup, wrap;
+    if (titleElem === null) {
+      return;
+    }
+
+    numElem = $('body.project .page-title');
+    title = titleElem.innerHTML;
+    if (numElem !== null) {
+      title = numElem.firstChild.textContent + " " + title;
+    }
+
+    buttonGroup = $("body.project .page-title .pull-right");
+    buttonGroup.insertBefore(createTimerLink(title, 'btn grouped'), buttonGroup.firstChild);
+  }
+
+  chrome.extension.sendMessage({type: 'activate'}, function (response) {
+    if (response.success) {
+      addLinkToDiscussion();
+    }
+  });
+
+}());

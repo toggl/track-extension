bd83262f52218ff5775a25bbb67e80c7966c3922
Tambet Masik <tambet@toggl.com>
2014-02-04 12:54:41 +0200

    Pivotaltracker refactoring

--- scripts/content/pivotal.js
+++ scripts/content/pivotal.js
@@ -1,61 +1,20 @@
 /*jslint indent: 2 */
-/*global window: false, document: false, chrome: false, $: false, createTag: false, createLink: false*/
+/*global $: false, document: false, togglbutton: false*/
+
 "use strict";
 
-(function () {
-  var isStarted = false;
+togglbutton.render('form.story:not(.toggl)', {observe: true}, function (elem) {
+  var link,
+    titleElem = $('textarea', elem),
+    container = $('.edit aside', elem);
 
-  function showNotice(text) {
-    var notice = createTag('div', 'message notice alert');
-    notice.appendChild(document.createTextNode(text));
-    $('.status').appendChild(notice);
-    window.setTimeout(function () {
-      notice.parentNode.removeChild(notice);
-    }, 2500);
+  if (titleElem === null || container === null) {
+    return;
   }
-
-  function addButtonListener(e) {
-    var elem = e.relatedNode, cont, btn, link;
-    if (elem.classList && (elem.classList.contains("story") || elem.classList.contains("model_details"))) {
-      cont = $(".edit aside", elem);
-
-      if ($('.toggl-button', cont)) {
-        btn = $('.toggl-button', cont);
-        btn.parentNode.removeChild(btn);
-      }
-
-      link = createLink('toggl-button pivotal');
-      link.addEventListener("click", function (e) {
-        var msg, btnText, notice;
-
-        if (isStarted) {
-          msg = {type: 'stop'};
-          btnText = 'Start timer';
-          notice = 'Toggl timer stopped';
-        } else {
-          msg = {
-            type: 'timeEntry',
-            description: $("textarea", elem).value
-          };
-          btnText = 'Stop timer';
-          notice = 'Toggl timer started';
-        }
-        chrome.extension.sendMessage(msg);
-        this.innerHTML = btnText;
-        showNotice(notice);
-        isStarted = !isStarted;
-      });
-      cont.appendChild(link);
-
-      // new button created - reset state
-      isStarted = false;
-    }
-  }
-
-  chrome.extension.sendMessage({type: 'activate'}, function (response) {
-    if (response.success) {
-      document.addEventListener('DOMNodeInserted', addButtonListener);
-    }
+  link = togglbutton.createTimerLink({
+    className: 'pivotal',
+    description: titleElem.value
   });
 
-}());
+  container.appendChild(link);
+});

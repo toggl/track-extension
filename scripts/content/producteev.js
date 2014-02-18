58201f2849621541e77f7ec1319cdf4cc9cccb48
Tambet Masik <tambet@toggl.com>
2014-02-18 09:18:01 +0200

    producteev jslint

--- scripts/content/producteev.js
+++ scripts/content/producteev.js
@@ -1,27 +1,29 @@
-/**
- * Created by lancelothk on 2/16/14.
- */
+/*jslint indent: 2 */
+/*global $: false, document: false, togglbutton: false*/
+/*Created by lancelothk on 2/16/14.*/
+
 "use strict";
 
 togglbutton.render('.td-attributes:not(.toggl)', {observe: true}, function (elem) {
-    var link,
-        taskActive = document.querySelector('.task.active'),
-        titleElem = $('.title > span', taskActive),
-        projectElem = $('.project-value', taskActive),
-        container = elem;//$('.td-attributes', elem);
+  var link, newDiv,
+    taskActive = document.querySelector('.task.active'),
+    titleElem = $('.title > span', taskActive),
+    projectElem = $('.project-value', taskActive),
+    container = elem;//$('.td-attributes', elem);
 
-    debugger;
-    if (titleElem === null || container === null) {
-        return;
-    }
-    link = togglbutton.createTimerLink({
-        className: 'producteev',
-        description: titleElem.title,
-        projectName: projectElem.title
-    });
+  debugger;
+  if (titleElem === null || container === null) {
+    return;
+  }
 
-    var newDiv = document.createElement('div');
-    newDiv.innerHTML = '&nbsp;&nbsp;&nbsp;';
-    newDiv.appendChild(link);
-    container.insertBefore(newDiv, container.firstChild);
+  link = togglbutton.createTimerLink({
+    className: 'producteev',
+    description: titleElem.title,
+    projectName: projectElem.title
+  });
+
+  newDiv = document.createElement('div');
+  newDiv.innerHTML = '&nbsp;&nbsp;&nbsp;';
+  newDiv.appendChild(link);
+  container.insertBefore(newDiv, container.firstChild);
 });

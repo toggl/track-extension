9e37659ca7fb89ea9f8d0cb1a0e29c4801479945
Tambet Masik <tambet.masik@gmail.com>
2014-02-22 19:41:51 +0200

    Reident bitbucket content script

--- scripts/content/bitbucket.js
+++ scripts/content/bitbucket.js
@@ -3,21 +3,21 @@
 'use strict';
 
 togglbutton.render('#issue-header', {}, function (elem) {
-	var link, description,
-		numElem = $('.issue-id'),
-		titleElem = $('#issue-title'),
-		projectElem = $('.repo-link');
+  var link, description,
+    numElem = $('.issue-id'),
+    titleElem = $('#issue-title'),
+    projectElem = $('.repo-link');
 
-	description = titleElem.innerText;
-		if (numElem !== null) {
-			description = numElem.innerText + " " + description;
-		}
+  description = titleElem.innerText;
+  if (numElem !== null) {
+    description = numElem.innerText + " " + description;
+  }
 
-	  link = togglbutton.createTimerLink({
-			className: 'bitbucket',
-			description: description,
-			projectName: projectElem && projectElem.textContent
-	  });
-	
-	$('#issue-header').appendChild(link);
+  link = togglbutton.createTimerLink({
+    className: 'bitbucket',
+    description: description,
+    projectName: projectElem && projectElem.textContent
+  });
+
+  $('#issue-header').appendChild(link);
 });

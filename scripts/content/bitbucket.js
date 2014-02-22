04c24bb29d25e1584fc2c1f3f2b1b9d5ed5bcb00
Ayman Bedair <aymanrb@gmail.com>
2014-02-22 17:04:16 +0200

    Added the bitbucket handling script

---
+++ scripts/content/bitbucket.js
@@ -0,0 +1,23 @@
+/*jslint indent: 2 */
+/*global $: false, document: false, togglbutton: false*/
+'use strict';
+
+togglbutton.render('#issue-header', {}, function (elem) {
+	var link, description,
+		numElem = $('.issue-id'),
+		titleElem = $('#issue-title'),
+		projectElem = $('.repo-link');
+
+	description = titleElem.innerText;
+		if (numElem !== null) {
+			description = numElem.innerText + " " + description;
+		}
+
+	  link = togglbutton.createTimerLink({
+			className: 'bitbucket',
+			description: description,
+			projectName: projectElem && projectElem.textContent
+	  });
+	
+	$('#issue-header').appendChild(link);
+});

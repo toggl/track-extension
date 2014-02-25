8a835a5c1a5e95db2b3acff8c6d96152b7c465c0
Ivo Rothschild <ivo@op3nvoice.com>
2014-02-25 17:58:09 -0500

    Added project selector for Pivotal
    In Pivotal, added a Toggle project selector next to the project name.

--- scripts/content/pivotal.js
+++ scripts/content/pivotal.js
@@ -3,6 +3,7 @@
 
 "use strict";
 
+
 togglbutton.render('form.story:not(.toggl)', {observe: true}, function (elem) {
   var link,
     titleElem = $('textarea', elem),
@@ -11,10 +12,33 @@ togglbutton.render('form.story:not(.toggl)', {observe: true}, function (elem) {
   if (titleElem === null || container === null) {
     return;
   }
+
+  var getProjectId = function() {
+      var sel = $('.toggl-project-select');
+      return sel ? sel.value : '';
+  };
+
   link = togglbutton.createTimerLink({
     className: 'pivotal',
-    description: titleElem.value
+    description: titleElem.value,
+    projectId: getProjectId
   });
 
   container.appendChild(link);
 });
+
+chrome.extension.sendMessage({type: 'activate'}, function (response) {
+    if (response.success && response.user && response.user.projects) {
+
+        var togglSelect = createProjectSelect(response.user,'toggl-project-select');
+
+        document.addEventListener('DOMNodeInserted', function() {
+            var titleSection = $('section.name h2');
+            if (titleSection) {
+                titleSection.appendChild(togglSelect);
+            }
+        });
+
+    }
+});
+

16f5e611778a2899e3e93fd1291ef15645ad8f4a
Steve Shaffer <WatchSteveDrum@gmail.com>
2014-04-16 08:50:40 -0700

    Create sifterapp.js
    sifterapp.com-specific implementation

---
+++ src/scripts/content/sifterapp.js
@@ -0,0 +1,23 @@
+/*jslint indent: 2 */
+/*global $: false, document: false, togglbutton: false*/
+'use strict';
+
+togglbutton.render('.subheader-content', {}, function (elem) {
+  var link, description,
+    numElem = $('.subheader-content h2 i'),
+    titleElem = $('.subheader-content h2'),
+    projectElem = $('#header h1');
+
+  description = titleElem.innerText;
+  if (numElem !== null) {
+    description = numElem.innerText + " " + description;
+  }
+
+  link = togglbutton.createTimerLink({
+    className: 'sifterapp',
+    description: description,
+    projectName: projectElem && projectElem.textContent
+  });
+
+  $('.subheader-content').appendChild(link);
+});

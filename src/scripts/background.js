627732e236fbf573785337058e6eadb7b7766094
Steve Shaffer <WatchSteveDrum@gmail.com>
2014-04-22 13:20:08 -0700

    added google docs to background

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -20,7 +20,7 @@ var TogglButton = {
     'worksection\\.com',
     'pivotaltracker\\.com',
     'producteev\\.com',
-    'redmine\\.org'].join('|')),
+    'docs\\.google\\.com'].join('|')),
   $curEntryId: null,
 
   checkUrl: function (tabId, changeInfo, tab) {

f4f933024537196db33590779bdc7f28ab8cc3c4
Tambet Masik <tambet@toggl.com>
2014-04-24 10:02:41 +0300

    JIRA content script refactoring

--- src/scripts/content/jira.js
+++ src/scripts/content/jira.js
@@ -1,103 +1,50 @@
 /*jslint indent: 2 */
-/*global window: false, document: false, chrome: false, $: false, createTag: false, createLink: false*/
-(function () {
-  "use strict";
-  var isStarted = false;
+/*global $: false, togglbutton: false, createTag: false*/
 
-  function createTimerLink(task, moreClass, tag) {
-    var link, msg, btnText;
+'use strict';
 
-    if (!tag) {
-      link = createLink('jira ' + moreClass);
-    } else {
-      link = createTag(tag, 'jira ' + moreClass);
-      link.appendChild(document.createTextNode('Start timer'));
-    }
-    link.addEventListener("click", function (e) {
-      e.preventDefault();
+togglbutton.render('#ghx-detail-issue:not(.toggl)', {observe: true}, function (elem) {
+  var link, description,
+    container = createTag('div', 'ghx-toggl-button'),
+    titleElem = $('[data-field-id="summary"]', elem),
+    numElem = $('.ghx-fieldname-issuekey a'),
+    projectElem = $('.ghx-project', elem);
 
-      if(isStarted) {
-        msg = {type: 'stop'};
-        btnText = 'Start timer';
-      } else {
-        msg = {
-          type: 'timeEntry',
-          description: task
-        };
-        btnText = 'Started...';
-      }
-
-      chrome.extension.sendMessage(msg);
-      $('span', link).innerHTML = btnText;
-      isStarted = !isStarted;
-    });
-
-    // new button created - reset state
-    isStarted = false;
-
-    return link;
+  description = titleElem.innerText;
+  if (numElem !== null) {
+    description = numElem.innerText + " " + description;
   }
 
-  function observeBoard() {
-    var target = document.querySelector('#ghx-detail-view');
-    var observer = new WebKitMutationObserver(function(mutations) {
-      mutations.forEach(function(mutation) {
-        if ($('.toggl-button')) {
-          return;
-        }
-
-        var titleElem = $('dd.ghx-fieldname-summary'), numElem, title,wrapSpan, button;
-        if (titleElem === null) {
-          return;
-        }
-
-        numElem = $('.ghx-fieldname-issuekey a');
-        title = titleElem.textContent;
-        if (numElem !== null) {
-          title = numElem.textContent + " " + title;
-        }
-
-        button = createTimerLink(title, 'aui-button ghx-actions aui-button-compact', 'button');
-        wrapSpan = createTag('span', 'toggl-button', button.textContent);
-        button.innerHTML = wrapSpan.outerHTML;
-        $(".ghx-controls").insertBefore(button, $(".ghx-controls").childNodes[0]);
-      });
-    });
-    var config = {childList: true, subtree: true};
-    observer.observe(target, config);
-  }
-
-  function addLinkToDiscussion() {
-    var titleElem = $('#summary-val'), numElem, title, wrapUl, wrapLi, wrapSpan, a;
-    if (titleElem === null) {
-      return;
-    }
-
-    numElem = $('.issue-link');
-    title = titleElem.textContent;
-    if (numElem !== null) {
-      title = numElem.getAttribute('data-issue-key') + " " + title;
-    }
-
-    wrapUl = createTag('ul', 'toggl toolbar-group');
-    wrapLi = createTag('li', 'toolbar-item');
-    wrapUl.appendChild(wrapLi);
-
-    a = createTimerLink(title, 'button toolbar-trigger');
-    wrapSpan = createTag('span', 'toggl-button', a.textContent);
-    a.innerHTML = wrapSpan.outerHTML;
-    wrapLi.appendChild(a);
-    $(".command-bar .toolbar-split-left").appendChild(wrapUl);
-  }
-
-  chrome.extension.sendMessage({type: 'activate'}, function (response) {
-    if (response.success) {
-      if ($('#gh')) {
-        observeBoard();
-      } else {
-        addLinkToDiscussion();
-      }
-    }
+  link = togglbutton.createTimerLink({
+    className: 'jira',
+    description: description,
+    projectName: projectElem && projectElem.innerText
   });
 
-}());
+  container.appendChild(link);
+  $('#ghx-detail-head').appendChild(container);
+});
+
+togglbutton.render('.issue-header-content:not(.toggl)', {observe: true}, function (elem) {
+  var link, description, ul, li,
+    numElem = $('#key-val', elem),
+    titleElem = $('#summary-val', elem),
+    projectElem = $('#project-name-val', elem);
+
+  description = titleElem.innerText;
+  if (numElem !== null) {
+    description = numElem.innerText + " " + description;
+  }
+
+  link = togglbutton.createTimerLink({
+    className: 'jira',
+    description: description,
+    projectName: projectElem && projectElem.innerText
+  });
+
+  ul = createTag('ul', 'toolbar-group');
+  li = createTag('li', 'toolbar-item');
+  li.appendChild(link);
+  ul.appendChild(li);
+  $('.toolbar-split-left').appendChild(ul);
+});

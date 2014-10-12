aa073feed1111cc81f8966b7b5581c229e85e05e
nakamura <nkmry333@gmail.com>
2014-10-12 15:15:00 +0900

    added folder name to description

--- src/scripts/content/toodledo.js
+++ src/scripts/content/toodledo.js
@@ -6,11 +6,15 @@
 togglbutton.render('.row:not(.toggl)', {observe: true}, function (elem) {
   var link,
     taskElem = $('.col0', elem),
-    goalElem = $('.col1024', elem);
+    goalElem = $('.col1024', elem),
+    folderElem = $('.col1', elem).firstChild,
+    folderName = folderElem && folderElem.textContent;
+
+  folderName = (folderName === "No Folder") ? "" : " - " + folderName;
 
   link = togglbutton.createTimerLink({
     className: 'toodledo',
-    description: taskElem.firstChild.textContent,
+    description: taskElem.firstChild.textContent + folderName,
     projectName: goalElem && goalElem.textContent
   });
 

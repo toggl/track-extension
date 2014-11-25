597baece3e532b4e9c38b4a9648d1e9281ad9b9f
IndrekV <indzuliin@gmail.com>
2014-11-25 23:29:13 +0200

    Add Toggl Button version to created_with

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -184,7 +184,7 @@ var TogglButton = {
           tags: timeEntry.tags || null,
           billable: timeEntry.billable || false,
           duration: -(start.getTime() / 1000),
-          created_with: timeEntry.createdWith || 'TogglButton',
+          created_with: timeEntry.createdWith || ("TogglButton" + chrome.runtime.getManifest().version),
           duronly: !TogglButton.$user.store_start_and_stop_time
         }
       };

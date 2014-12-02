ab06b021b19da2ebecec184de90a6484d7996dc3
RedDwarf <ivica@orioly.com>
2014-12-02 02:33:43 +0100

    adding button to tfs

--- src/scripts/content/TFS.js
+++ src/scripts/content/TFS.js
@@ -3,25 +3,32 @@
 
 'use strict';
 
-togglbutton.render('.workitem-tool-bar:not(.toggl)', {observe: true}, function (elem) {
+togglbutton.render('.toolbar:not(.toggl)', {observe: true}, function (elem) {
 
 window.setTimeout("createTFSButton()", 1000);
 
 });
-
+var oldHTML = "";
 function createTFSButton(){
 
 
 	var link,
     projectElem = $('.project-name'),
-    descriptionElem = document.querySelectorAll('.workitem-tool-bar')
+    descriptionElem = document.querySelectorAll('.toolbar')
 
   link = togglbutton.createTimerLink({
     className: 'trello',
-    description: descriptionElem[descriptionElem.length-1].parentNode.parentNode.parentNode.firstChild.firstChild.innerText,
+    description: $('.info-text-wrapper > a').innerText + " " + $('.info-text-wrapper > span').innerText, //descriptionElem[descriptionElem.length-1].parentNode.parentNode.parentNode.firstChild.firstChild.innerText,
     projectName: projectElem.innerText
   });
 descriptionElem[descriptionElem.length-1].firstChild.appendChild(link);
+var bla = createTag('div', 'empty')
+var a = createTag('a', 'emptylink')
+$('.workitem-info-bar').insertBefore(bla, $(".info-text-wrapper"));
+$('.empty').appendChild(a);
+$('.emptylink').setAttribute("href", "#");
+$('.emptylink').innerHTML = "&nbsp;";
+$('.empty').setAttribute("style", "float:left");
 
 }
 

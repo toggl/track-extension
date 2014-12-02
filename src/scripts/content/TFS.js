4b90ef0ffcb862ced65fe5e4fb040b8a8b32b314
Ivica Vinkovic <ivinkovi@gmail.com>
2014-12-02 16:05:53 +0100

    adding suport for local tfs instance

--- src/scripts/content/TFS.js
+++ src/scripts/content/TFS.js
@@ -8,27 +8,48 @@ togglbutton.render('.toolbar:not(.toggl)', {observe: true}, function (elem) {
 window.setTimeout("createTFSButton()", 1000);
 
 });
-var oldHTML = "";
+
 function createTFSButton(){
-
-
 	var link,
     projectElem = $('.project-name'),
-    descriptionElem = document.querySelectorAll('.toolbar')
+    togglElem = document.querySelectorAll('.toolbar'),
+    descriptionElem = document.querySelectorAll('.info-text-wrapper')
+	
 
   link = togglbutton.createTimerLink({
-    className: 'trello',
-    description: $('.info-text-wrapper > a').innerText + " " + $('.info-text-wrapper > span').innerText, //descriptionElem[descriptionElem.length-1].parentNode.parentNode.parentNode.firstChild.firstChild.innerText,
+    className: 'tfsTogglButton',
+    description: descriptionElem[descriptionElem.length-1].childNodes[0].innerText + " " + descriptionElem[descriptionElem.length-1].childNodes[1].innerText ,
     projectName: projectElem.innerText
   });
-descriptionElem[descriptionElem.length-1].firstChild.appendChild(link);
+  
+togglElem[togglElem.length-1].firstChild.appendChild(link);
 var bla = createTag('div', 'empty')
 var a = createTag('a', 'emptylink')
-$('.workitem-info-bar').insertBefore(bla, $(".info-text-wrapper"));
-$('.empty').appendChild(a);
-$('.emptylink').setAttribute("href", "#");
-$('.emptylink').innerHTML = "&nbsp;";
-$('.empty').setAttribute("style", "float:left");
-
+var findWhere = document.querySelectorAll('.workitem-info-bar') 
+if(findWhere[findWhere.length-1].innerHTML.indexOf('class="empty"') == -1){
+findWhere[findWhere.length-1].insertBefore(bla, descriptionElem[descriptionElem.length-1]);
+var findNew = document.querySelectorAll('.empty') 
+findNew[findNew.length-1].appendChild(a);
+var findNewLink = document.querySelectorAll('.emptylink')
+findNewLink[findNewLink.length-1].setAttribute("href", "javascript:void()");
+findNewLink[findNewLink.length-1].innerHTML = "&nbsp;";
+findNew[findNew.length-1].setAttribute("style", "float:left");
 }
+var findLastInput = document.querySelectorAll('.wit-font-size-large > div > div > input')
+findLastInput[findLastInput.length-1].addEventListener('blur', reCreateButton, false);
+
+if (togglElem[togglElem.length-1].innerHTML.indexOf('class="toggl-button tfsTogglButton"') == -1){window.setTimeout("reCreateButton()", 1000);}
+}
+
+function reCreateButton(){
+	var togglElem =  document.querySelectorAll('.toolbar')
+	if(togglElem[togglElem.length-1].innerHTML.indexOf('class="toggl-button tfsTogglButton"') > -1){ 
+		togglElem[togglElem.length-1].innerHTML = togglElem[togglElem.length-1].innerHTML.replace('<a class="toggl-button tfsTogglButton" href="#">Start timer</a>','');
+		togglElem[togglElem.length-1].innerHTML = togglElem[togglElem.length-1].innerHTML.replace('<a class="toggl-button tfsTogglButton" href="#">Stop timer</a>','');
+	}
+	var findLastInput = document.querySelectorAll('.wit-font-size-large > div > div > input')
+	findLastInput[findLastInput.length-1].removeEventListener ("blur", reCreateButton, false);
+	window.setTimeout("createTFSButton()", 100);
+}
+
 

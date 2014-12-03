dd5ec58a5555e3a2b0514e1e63676f1a01d6ca44
Dan <dan@teamworkpm.net>
2014-12-03 11:24:31 +0000

    Support for View Page

--- src/scripts/content/teamworkpm.js
+++ src/scripts/content/teamworkpm.js
@@ -3,10 +3,9 @@
 
 'use strict';
 
-console.log("Load Toggl for Teamwork");
-
+// Tasks listing page in project
 togglbutton.render('div.taskRHS:not(.toggl)', {observe: true}, function (elem) {
-  var link, className = 'huh', container = $('.taskUnusedOps', elem), spanTag;
+  var link, className = 'huh', container = $('.taskIcons', elem), spanTag, projectName;
 
   if (container === null) {
     return;
@@ -15,12 +14,17 @@ togglbutton.render('div.taskRHS:not(.toggl)', {observe: true}, function (elem) {
   if( $('.taskName', elem) === null ) {
     return;
   }
+
+  if( $("#projectName") ) {
+    projectName = $("#projectName").innerHTML
+  }
+
   var desc = $('.taskName', elem).textContent;
 
   link = togglbutton.createTimerLink({
     className: 'teamworkpm',
     description: desc,
-    projectName: $("#projectName").innerHTML
+    projectName: projectName
   });
 
   link.classList.add( className );
@@ -30,7 +34,7 @@ togglbutton.render('div.taskRHS:not(.toggl)', {observe: true}, function (elem) {
     // Run through and hide all others
     var i, len, elems = document.querySelectorAll(".toggl-button");
     for (i = 0, len = elems.length; i < len; i += 1) {
-      elems[i].classList.add('huh');
+      elems[i].classList.add('huh');    
     }
 
     if (link.classList.contains( className ) ) {
@@ -41,8 +45,37 @@ togglbutton.render('div.taskRHS:not(.toggl)', {observe: true}, function (elem) {
   });
 
   spanTag = document.createElement("span");
+  spanTag.classList.add("toggl-span");
   link.style.width = 'auto';
-  link.style.paddingLeft = '22px';
+  link.style.paddingLeft = '20px';
+  link.setAttribute( "title" , "Toggl Timer" );
   spanTag.appendChild(link);  
-  container.insertBefore( spanTag, container.firstChild);
+  container.insertBefore( spanTag, container.lastChild);
+});
+
+// Tasks View Page
+togglbutton.render('div#Task div.titleHolder ul.options:not(.toggl)', {observe: true}, function (elem) {
+  var link, liTag, titleEl, projectName;
+  liTag = document.createElement("li");
+  liTag.classList.add("toggl-li");
+
+  titleEl = document.getElementById("Task");  
+  var desc = titleEl.getAttribute("data-taskname");
+
+  if( $("#projectName") ) {
+    projectName = $("#projectName").innerHTML
+  }
+
+  link = togglbutton.createTimerLink({
+    className: 'teamworkpm',
+    description: desc,
+    projectName: projectName
+  });
+
+  link.classList.add("btn");
+  link.classList.add("btn-default");
+  link.setAttribute( "title" , "Toggl Timer" );  
+  liTag.appendChild(link);  
+  elem.insertBefore( liTag, elem.firstChild);
+ 
 });

ee9c03f3acd6c455c789cb085c14bd1144ec1f93
IndrekV <indzuliin@gmail.com>
2015-01-21 15:17:36 +0200

    Fixed double start links in redmine

--- src/scripts/content/redmine.js
+++ src/scripts/content/redmine.js
@@ -2,7 +2,7 @@
 /*global $: false, document: false, togglbutton: false*/
 'use strict';
 
-togglbutton.render('body.controller-issues.action-show h2', {}, function (elem) {
+togglbutton.render('body.controller-issues.action-show h2:not(.toggl)', {}, function (elem) {
   var link, description,
     numElem = $('h2'),
     titleElem = $('.subject h3'),

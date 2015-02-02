776c1edb7cc151b9f114f3674ac165be7acf0d30
IndrekV <indzuliin@gmail.com>
2015-02-02 12:07:25 +0200

    Fixed Todoist description parse, closes #200

--- src/scripts/content/todoist.js
+++ src/scripts/content/todoist.js
@@ -4,12 +4,22 @@
 'use strict';
 
 togglbutton.render('.task_item .content:not(.toggl)', {observe: true}, function (elem) {
-  var link, container = $('.text', elem),
+  var link, descFunc, container = $('.text', elem),
     projectElem = $('.project_link');
+
+  descFunc = function () {
+    var clone = container.cloneNode(true);
+
+    while (clone.children.length) {
+      clone.children[0].remove();
+    }
+
+    return clone.textContent.trim();
+  };
 
   link = togglbutton.createTimerLink({
     className: 'todoist',
-    description: container.textContent.replace("Stop timer", ""),
+    description: descFunc,
     projectName: projectElem && projectElem.textContent
   });
 

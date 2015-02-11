392cd8aa5bdd0324562dc5636bd1f6199cdd509a
IndrekV <indzuliin@gmail.com>
2015-02-11 15:49:47 +0200

    Fixed duplicate link in drupal

--- src/scripts/content/drupalorg.js
+++ src/scripts/content/drupalorg.js
@@ -2,7 +2,7 @@
 /*global $: false, document: false, togglbutton: false*/
 'use strict';
 
-togglbutton.render('body.node-type-project-issue #tabs ul', {}, function (elem) {
+togglbutton.render('body.node-type-project-issue #tabs ul:not(.toggl)', {}, function (elem) {
 
   var link = togglbutton.createTimerLink({
     className: 'drupalorg',

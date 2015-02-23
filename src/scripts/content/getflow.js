67eb7d974c62fc532e8856634e7f34e3863ce7b3
Juan Ferreras <juan@flioh.com>
2015-02-23 14:46:19 -0300

    Fixed GetFlow support
    Now it detects the project too, by removing the empty white space that
    it was fetching.
    Updated the styles.css, and README.md

--- src/scripts/content/getflow.js
+++ src/scripts/content/getflow.js
@@ -1,16 +1,21 @@
 /*jslint indent: 2 */ 
 /*global $: false, document: false, togglbutton: false*/ 
 'use strict'; 
-togglbutton.render('.content-list-item-label content-list-item:not(.toggl)', {observe: true}, function (elem) { 
+togglbutton.render('.task-list-section-collection-list li:not(.toggl)', {observe: true}, function (elem) {
   var link, 
-    description = $('.content-list-item-name-wrapper', elem).text(), 
-    project = $('.context-header-title .copy').text(); 
+  	spanTag,
+    container = $('.task-list-item-details', elem);
 
-  link = togglbutton.createTimerLink({ 
-    className: 'getflow', 
-    description: description, 
-    projectName: project 
-  }); 
+  if (container === null) {
+    return;
+  }
 
-  $('.content-list-item-name-detail-wrapper').prepend(link); 
+  // Have to remove the empty character projectName gets at the end
+  link = togglbutton.createTimerLink({
+    className: 'getflow',
+    description: $('.content-list-item-name-wrapper', elem).textContent,
+    projectName: $('.context-header-title .copy').innerText.slice(0, - 1)
+  });
+
+  container.appendChild(link);
 });

e81e823b4ba311c67ed4f5b1e21e40413c6aa723
IndrekV <indzuliin@gmail.com>
2015-03-06 15:40:35 +0200

    Added support for Basecamphq.com, closes #229

--- src/scripts/content/basecamp.js
+++ src/scripts/content/basecamp.js
@@ -3,6 +3,7 @@
 
 'use strict';
 
+// Basecamp Next
 togglbutton.render('section.todos li.todo:not(.toggl)', {observe: true}, function (elem) {
   var link, behavior = 'hover_content',
     container = $('.pill', elem), spanTag;
@@ -28,4 +29,28 @@ togglbutton.render('section.todos li.todo:not(.toggl)', {observe: true}, functio
 
   spanTag = document.createElement("span");
   container.parentNode.appendChild(spanTag.appendChild(link));
+});
+
+
+// Basecamp Classic
+togglbutton.render('.items_wrapper .item > .content:not(.toggl)', {observe: true}, function (elem) {
+  var link, behavior = 'selectable_target', spanTag;
+
+  link = togglbutton.createTimerLink({
+    className: 'basecamphq',
+    description: $('.content', elem).textContent.trim(),
+    projectName: (!!$(".project")) ? ($(".project > title") || $(".project > header > h1 > a")).innerHTML : ""
+  });
+
+  link.setAttribute('data-behavior', behavior);
+  link.addEventListener('click', function (e) {
+    if (link.getAttribute('data-behavior') === '') {
+      link.setAttribute('data-behavior', behavior);
+    } else {
+      link.setAttribute('data-behavior', '');
+    }
+  });
+
+  spanTag = document.createElement("span");
+  $(".content", elem).appendChild(spanTag.appendChild(link));
 });

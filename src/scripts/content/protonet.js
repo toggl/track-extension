f4e70e0f81280e9903a887571886cb2793ac90fd
Christian Seel <cs@chsmedien.de>
2015-05-10 16:48:26 +0200

    added protonet integration

---
+++ src/scripts/content/protonet.js
@@ -0,0 +1,25 @@
+/*jslint indent: 2 */
+/*global $: false, document: false, togglbutton: false*/
+'use strict';
+
+togglbutton.render('.todo-details:not(.toggl)', {observe: true}, function (elem) {
+  var link,
+    //container = createTag('div', 'actions'),
+    title = $('.name', elem).textContent,
+    href = $('.name', elem).getAttribute('href'),
+    project = $('ul.breadcrumb li:first-child a', elem).textContent,
+    id = href.split("/").pop(-1),
+    description = '#' + id + ': ' + title;
+
+  link = togglbutton.createTimerLink({
+    className: 'protonet',
+    description: description,
+    projectName: project
+  });
+  
+  console.log(description, project);
+    
+  //container.appendChild(link);
+  //$('.todo-details').appendChild(container);
+  $('.actions').appendChild(link);
+});

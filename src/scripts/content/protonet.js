27616f090c22f55998d08b935a26ea543ab3c123
Christian Seel <cs@chsmedien.de>
2015-05-11 09:15:38 +0200

    added toggle button for topic messenges

--- src/scripts/content/protonet.js
+++ src/scripts/content/protonet.js
@@ -4,10 +4,9 @@
 
 togglbutton.render('.todo-details:not(.toggl)', {observe: true}, function (elem) {
   var link,
-    //container = createTag('div', 'actions'),
     title = $('.name', elem).textContent,
     href = $('.name', elem).getAttribute('href'),
-    project = $('ul.breadcrumb li:first-child a', elem).textContent,
+    project = $('ul.breadcrumb li:nth-child(3) a', elem).textContent,
     id = href.split("/").pop(-1),
     description = '#' + id + ': ' + title;
 
@@ -17,9 +16,35 @@ togglbutton.render('.todo-details:not(.toggl)', {observe: true}, function (elem)
     projectName: project
   });
   
-  console.log(description, project);
-    
-  //container.appendChild(link);
-  //$('.todo-details').appendChild(container);
-  $('.actions').appendChild(link);
+  $('.actions', elem).appendChild(link);
+});
+
+
+togglbutton.render('.detail-content-wrapper li.meep.Meep:not(.toggl)', {observe: true}, function (elem) {
+  var link, project,
+    project = $('.detail-content-wrapper .name').textContent,
+    title = $('.message', elem).textContent,
+    href = window.location.href + $('.meta .time', elem).getAttribute('href'),
+    id = href.split("meep_id=").pop(-1),
+    description = 'Nachricht #' + id + ' (' + href + ')',
+    destroyLink = $('.remove', elem);
+
+  link = togglbutton.createTimerLink({
+    className: 'protonet',
+    description: description,
+    projectName: project
+  });
+  
+  link.style.position = 'relative';
+  link.style.top = '1px';
+  link.style.marginTop = '-1px';
+  link.style.marginLeft = '4px';
+  link.style.paddingTop = '1px';
+  link.style.opacity = '0.5';
+  link.style.fontSize = '17px';
+  link.style.transform = 'scale(0.8)';
+  link.style.webkitFilter = 'grayscale(1)';
+  
+  link.classList.add('tiny');
+  destroyLink.parentNode.insertBefore(link, destroyLink);
 });

67a354a7b79f0300e1796ed42c3ce8572ed087d7
Sergey Ivanov <ivanovserg990@gmail.com>
2015-05-31 16:47:54 -0400

    Add window.onload; correctly detect description

--- src/scripts/content/gqueues.js
+++ src/scripts/content/gqueues.js
@@ -2,36 +2,42 @@
 /*global $: false, document: false, togglbutton: false*/
 'use strict';
 
-var list = $("#gqItemList").children;
-console.log(list.length);
-var ids = []
-for (var i=0; i<list.length; i++) {
-  ids.push(list[i].id)
-}
-
-for (i=0; i<ids.length; i++) {
-  if (ids[i] === "gqItemQueueEmpty") {
-    console.log('No items in gq')
+// GQueues uses 1.5.2 jQuery
+window.onload = function() {
+  // retrieve ids of items
+  // var items = document.getElementById('gqItemList').getElementsByClassName('gq-item-row');
+ 
+  var items = $("#gqItemList").children;
+  console.log(items);
+  var ids = []
+  for (var i=0; i<items.length; i++) {
+    ids.push(items[i].id)
   }
-  else {
-    var wrapper = '#' + ids[i]; 
-    console.log(wrapper);
-    togglbutton.render(wrapper + ':not(.toggl)', {observe: true}, function (elem) {
-      console.log('About to add Link');
+  console.log(ids);
 
+  // add timer to each item
+  for (var i=0; i<ids.length; i++) {
+    if (ids[i] === "gqItemQueueEmpty") {
+      console.log('No items in gq')
+    }
+    else {
+      var wrapper = '#' + ids[i]; 
+      console.log(wrapper);
+      togglbutton.render(wrapper + ':not(.toggl)', {observe: true}, function (elem) {
+        console.log('About to add Link');
 
+        var link, description;
+        description = document.getElementById(elem.id).getElementsByClassName('gq-i-description')[0].innerHTML;
+        console.log(description);
 
-      var link,
-        description = $(wrapper + ' .gq-i-description', elem).text;
-      console.log(description);
+        link = togglbutton.createTimerLink({
+          className: 'gqueues',
+          description: 'gqueues-' + description,
+          projectName: 'gqueues-' + description
+        });
 
-      link = togglbutton.createTimerLink({
-        className: 'gqueues',
-        description: description,
-        projectName: 'gqueues-'
+        elem.appendChild(link);
       });
-
-      elem.appendChild(link);
-    });
+    }
   }
 }

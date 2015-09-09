dbf4c6fafb5a922a71fd4fdd227646db2c8653f3
IndrekV <indzuliin@gmail.com>
2015-09-09 14:36:52 +0300

    Fixed hour calculation for durations longer than 24 hours

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -90,7 +90,7 @@ var PopUp = {
   msToTime: function (duration) {
     var seconds = parseInt((duration / 1000) % 60, 10),
       minutes = parseInt((duration / (1000 * 60)) % 60, 10),
-      hours = parseInt((duration / (1000 * 60 * 60)) % 24, 10);
+      hours = parseInt((duration / (1000 * 60 * 60)), 10);
 
     hours = (hours < 10) ? "0" + hours : hours;
     minutes = (minutes < 10) ? "0" + minutes : minutes;

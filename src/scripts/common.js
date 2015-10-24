e82dd47602838bf5797949595c74d5ddaf6b358b
Vladyslav Sitalo <root@stvad.org>
2015-10-24 16:28:53 +0300

    Remove redundunt function from common.js

--- src/scripts/common.js
+++ src/scripts/common.js
@@ -153,13 +153,6 @@ var togglbutton = {
     return result + selected.text;
   },
 
-  pomodoroAlarmStop: function (alarm) {
-    console.log("on alarm event");
-    if(alarm.name === 'PomodoroTimer') {
-      chrome.extension.sendMessage({type: 'stop'}, togglbutton.addEditForm);
-    }
-  },
-
   addEditForm: function (response) {
     togglbutton.hasTasks = response.hasTasks;
     if (response === null || !response.showPostPopup) {

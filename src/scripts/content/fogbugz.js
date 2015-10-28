652ad1b5328d007731359ff708a7e78b532fa9dd
IndrekV <indzuliin@gmail.com>
2015-10-28 11:29:28 +0200

    linting fogbugz.js

--- src/scripts/content/fogbugz.js
+++ src/scripts/content/fogbugz.js
@@ -4,18 +4,18 @@
 'use strict';
 
 togglbutton.render('section.case:not(.toggl)', {observe: true}, function (elem) {
-    var link, container = createTag('div', 'control'),
-        titleElem = $('.top h1', elem),
-        projectElem = $('.top .case-header-info a'),
-        caseNoElem = $('.top .left a.case'),
-        controlsElem = $('nav .controls');
+  var link, container = createTag('div', 'control'),
+    titleElem = $('.top h1', elem),
+    projectElem = $('.top .case-header-info a'),
+    caseNoElem = $('.top .left a.case'),
+    controlsElem = $('nav .controls');
 
-    link = togglbutton.createTimerLink({
-        className: 'fogbugz',
-        description: "[" + caseNoElem.innerText + "] " + titleElem.innerText,
-        projectName: projectElem.innerText
-    });
+  link = togglbutton.createTimerLink({
+    className: 'fogbugz',
+    description: "[" + caseNoElem.innerText + "] " + titleElem.innerText,
+    projectName: projectElem.innerText
+  });
 
-    container.appendChild(link);
-    controlsElem.appendChild(container);
+  container.appendChild(link);
+  controlsElem.appendChild(container);
 });

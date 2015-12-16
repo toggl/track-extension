61f79d14f9dc6418f55462b6e01e80bdb20c0e0d
Tomek Marcinkowski <tomasz.marcinkowski@rst.com.pl>
2015-12-16 09:20:19 +0100

    TargetProcess: minor changes, task button placement adjusted

--- src/scripts/content/targetprocess.js
+++ src/scripts/content/targetprocess.js
@@ -5,12 +5,9 @@
 // main entity button
 togglbutton.render('div.general-info:not(.toggl)', {observe: true}, function (elem) {
     var titleElement = $('.i-role-title', elem);
+    var entityIdName = $('.entity-id a', elem).textContent + ' ' + titleElement.textContent;
 
-    var link, entityIdName;
-
-    entityIdName = $('.entity-id a', elem).textContent + ' ' + titleElement.textContent;
-
-    link = togglbutton.createTimerLink({
+    var link = togglbutton.createTimerLink({
         className:   'targetprocess',
         description: entityIdName,
         projectName: entityIdName
@@ -21,19 +18,19 @@ togglbutton.render('div.general-info:not(.toggl)', {observe: true}, function (el
 
 // entity's task buttons
 togglbutton.render('.tau-list__table__row:not(.toggl)', {observe: true}, function (elem) {
-    var link, entityIdName;
-    var entityId = '#' + $('.tau-list__table__cell-id', elem).textContent.trim();
-    var entityTitle = $('.tau-list__table__cell-name', elem).textContent.trim();
+    var taskId      = '#' + $('.tau-list__table__cell-id', elem).textContent.trim();
+    var taskTitle   = $('.tau-list__table__cell-name', elem).textContent.trim();
+    var entityTitle = $('.ui-title_type_main .i-role-title').textContent.trim();
+    var entityId    = $('.ui-title_type_main .entity-id').textContent.trim();
 
-    entityIdName = entityId + ' ' + entityTitle;
-
-    link = togglbutton.createTimerLink({
+    var link = togglbutton.createTimerLink({
         className:   'targetprocess',
-        description: entityIdName,
-        projectName: entityIdName,
+        description: taskId + ' ' + taskTitle,
+        projectName: entityId + ' ' + entityTitle,
         buttonType:  'minimal'
     });
 
-    $('.tau-list__table__cell-state', elem).appendChild(link);
+    var buttonPlaceholder = $('.tau-list__table__cell-state', elem);
+    buttonPlaceholder.insertBefore(link, buttonPlaceholder.firstChild);
 });
 

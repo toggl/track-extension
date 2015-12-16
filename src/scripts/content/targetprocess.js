ac67af67a6e4cf104413886e28b115a258816249
Tomek Marcinkowski <tomasz.marcinkowski@rst.com.pl>
2015-12-16 08:33:24 +0100

    TargetProcess: task buttons

--- src/scripts/content/targetprocess.js
+++ src/scripts/content/targetprocess.js
@@ -2,7 +2,8 @@
 /*global $: false, document: false, togglbutton: false*/
 'use strict';
 
-togglbutton.render('div.general-info:not(.toggl)', {observe: true}, function(elem) {
+// main entity button
+togglbutton.render('div.general-info:not(.toggl)', {observe: true}, function (elem) {
     var titleElement = $('.i-role-title', elem);
 
     var link, entityIdName;
@@ -10,11 +11,29 @@ togglbutton.render('div.general-info:not(.toggl)', {observe: true}, function(ele
     entityIdName = $('.entity-id a', elem).textContent + ' ' + titleElement.textContent;
 
     link = togglbutton.createTimerLink({
-        className: 'targetprocess',
+        className:   'targetprocess',
         description: entityIdName,
         projectName: entityIdName
     });
 
     titleElement.parentElement.appendChild(link);
+});
+
+// entity's task buttons
+togglbutton.render('.tau-list__table__row:not(.toggl)', {observe: true}, function (elem) {
+    var link, entityIdName;
+    var entityId = '#' + $('.tau-list__table__cell-id', elem).textContent.trim();
+    var entityTitle = $('.tau-list__table__cell-name', elem).textContent.trim();
+
+    entityIdName = entityId + ' ' + entityTitle;
+
+    link = togglbutton.createTimerLink({
+        className:   'targetprocess',
+        description: entityIdName,
+        projectName: entityIdName,
+        buttonType:  'minimal'
+    });
+
+    $('.tau-list__table__cell-state', elem).appendChild(link);
 });
 

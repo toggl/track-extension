eefb5e18ffce0649860a3fa681daa8958868a81c
IndrekV <indzuliin@gmail.com>
2016-01-04 16:34:29 +0200

    Handle error on time entry update

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -63,7 +63,7 @@ var PopUp = {
         }
       } else if (request.type === "login") {
         PopUp.$error.style.display = 'block';
-      } else if (!!response.type && (response.type === "New Entry" || response.type === "Update")) {
+      } else if (!!response.type && (response.type === "New Entry" || response.type === "Update")) {
         PopUp.showError();
       }
     });

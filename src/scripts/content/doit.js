d8d5270a9215947545a3038677e49f56e4795f5d
Hikaru Watanabe <hikaru@paperboy.co.jp>
2016-02-02 22:54:53 +0900

    Revert "show timer on subtask list" find bugs
    This reverts commit 7f956b58912f651d19383655e56598d3dc9f3df2.

--- src/scripts/content/doit.js
+++ src/scripts/content/doit.js
@@ -68,27 +68,3 @@ togglbutton.render('li.task:not(.toggl)', {observe: true}, function (elem) {
   });
   titleElem.parentElement.appendChild(link);
 });
-
-togglbutton.render('li.subtask:not(.toggl)', {observe: true}, function (elem) {
-  var titleElem   = $('div.title > span.title-text',elem);
-  var projectElem = $('div.task-attr > div.project');
-
-  var  description = titleElem != null ? titleElem.innerHTML :''; 
-  var  project = projectElem != null ? projectElem.innerHTML : '';
-
-  if ( description != '' ) {
-    titleElem.innerHTML = description + ' ' ;
-  }
- 
-  if ( project != '' ) {
-    project = project.substring(1);  // remove '#'
-  }
-
-  var link = togglbutton.createTimerLink({
-    className: 'doit',
-    buttonType: 'minimal',
-    description: description,
-    projectName: project
-  });
-  titleElem.parentElement.appendChild(link);
-});

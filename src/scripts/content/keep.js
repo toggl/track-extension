6ec37a44fa0fe83ddc1196148b745ded2d4fee3c
IndrekV <indzuliin@gmail.com>
2016-02-16 11:10:33 +0200

    linting keep.js

--- src/scripts/content/keep.js
+++ src/scripts/content/keep.js
@@ -4,8 +4,8 @@
 
 togglbutton.render('.IZ65Hb-TBnied:not(.toggl)', {observe: true}, function (elem) {
   var link,
-      toolbar = $('.IZ65Hb-INgbqf', elem),
-      description = $('.IZ65Hb-YPqjbf:not(.LwH6nd)', elem).innerText;
+    toolbar = $('.IZ65Hb-INgbqf', elem),
+    description = $('.IZ65Hb-YPqjbf:not(.LwH6nd)', elem).innerText;
 
   link = togglbutton.createTimerLink({
     className: 'keep',

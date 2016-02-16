fe727748bb5eba0d21cc3aa612f201f93483ded5
IndrekV <indzuliin@gmail.com>
2016-02-16 11:10:05 +0200

    Made Keep timer link minmal

--- src/scripts/content/keep.js
+++ src/scripts/content/keep.js
@@ -4,14 +4,14 @@
 
 togglbutton.render('.IZ65Hb-TBnied:not(.toggl)', {observe: true}, function (elem) {
   var link,
-      container = createTag('div', 'keep-tb-wrapper'),
       toolbar = $('.IZ65Hb-INgbqf', elem),
       description = $('.IZ65Hb-YPqjbf:not(.LwH6nd)', elem).innerText;
+
   link = togglbutton.createTimerLink({
     className: 'keep',
+    buttonType: 'minimal',
     description: description,
     projectName: ""
   });
-  container.appendChild(link);
-  toolbar.appendChild(container);
+  toolbar.appendChild(link);
 });

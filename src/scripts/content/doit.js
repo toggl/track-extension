5ef4c90f344c5a306f651a7d5fa8019ddb7c20b5
IndrekV <indzuliin@gmail.com>
2016-02-18 12:17:33 +0200

    Linting doit.im

--- src/scripts/content/doit.js
+++ src/scripts/content/doit.js
@@ -5,16 +5,17 @@
 // Add button to task detail view
 togglbutton.render('[id^=taskview_task_]:not(.toggl)', {observe: true}, function (elem) {
   var link,
-    titleElem   = $('h3 > span.title.ng-binding',elem),
-    projectElem = $('div > div.item.project.ng-binding',elem),
-    description = titleElem != null ? titleElem.innerHTML :'',
-    project = projectElem != null ? projectElem.innerHTML : '';
+    titleElem   = $('h3 > span.title.ng-binding', elem),
+    projectElem = $('div > div.item.project.ng-binding', elem),
+    description = !!titleElem ? titleElem.innerHTML : '',
+    project = !!projectElem ? projectElem.innerHTML : '',
+    firstOp;
 
-  if ( description != '' ) {
-    titleElem.innerHTML = description + ' ' ;
+  if (description !== '') {
+    titleElem.innerHTML = description + ' ';
   }
- 
-  if ( project != '' ) {
+
+  if (project !== '') {
     project = project.substring(1);  // remove '#'
   }
 
@@ -23,17 +24,18 @@ togglbutton.render('[id^=taskview_task_]:not(.toggl)', {observe: true}, function
     description: description,
     projectName: project
   });
-  var firstOp = $('li.task-op').children[0]; 
-  $('li.task-op').insertBefore(link,firstOp);
+
+  firstOp = $('li.task-op').children[0];
+  $('li.task-op').insertBefore(link, firstOp);
 });
 
 // Add button to popup form ( like when editing project )
 togglbutton.render('#taskform > div > div.taskform-main:not(.toggl)', {observe: true}, function (elem) {
   var link,
-    titleElem   = $('li.meta.task-title > div > div > div.textarea-tmp',elem),
-    projectElem = $('li.meta.task-project.smart > div > div > ul > li.token > div',elem),
-    description = titleElem != null ? titleElem.innerHTML :'',
-    project = projectElem != null ? projectElem.innerHTML : '';
+    titleElem   = $('li.meta.task-title > div > div > div.textarea-tmp', elem),
+    projectElem = $('li.meta.task-project.smart > div > div > ul > li.token > div', elem),
+    description = !!titleElem ? titleElem.innerHTML : '',
+    project = !!projectElem ? projectElem.innerHTML : '';
 
   link = togglbutton.createTimerLink({
     className: 'doit',
@@ -47,23 +49,23 @@ togglbutton.render('#taskform > div > div.taskform-main:not(.toggl)', {observe: 
 // Add button to tasks in the list
 togglbutton.render('li.task:not(.toggl)', {observe: true}, function (elem) {
   var link,
-    titleElem   = $('div.title > a.link-title',elem),
-    projectElem = $('div.title > a.project',elem),
-    description = titleElem != null ? titleElem.innerHTML :'',
+    titleElem   = $('div.title > a.link-title', elem),
+    projectElem = $('div.title > a.project', elem),
+    description = !!titleElem ? titleElem.innerHTML : '',
     project,
     projectHeader = $('#project_info:not(.ng-hide)');
 
   if (!!projectHeader) {
     project = document.querySelector("#project_info span.title").textContent;
   } else {
-    project =projectElem != null ? projectElem.innerHTML : ''
-    if ( project != '' ) {
+    project = !!projectElem ? projectElem.innerHTML : '';
+    if (project !== '') {
       project = project.substring(1);  // remove '#'
     }
   }
 
-  if ( description != '' ) {
-    titleElem.innerHTML = description + ' ' ;
+  if (description !== '') {
+    titleElem.innerHTML = description + ' ';
   }
 
   link = togglbutton.createTimerLink({
@@ -72,5 +74,6 @@ togglbutton.render('li.task:not(.toggl)', {observe: true}, function (elem) {
     description: description,
     projectName: project
   });
+
   titleElem.parentElement.appendChild(link);
 });

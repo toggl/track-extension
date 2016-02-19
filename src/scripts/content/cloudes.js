25da456b375130f69bf2ce27746f5cd51f464a64
IndrekV <indzuliin@gmail.com>
2016-02-19 11:43:18 +0200

    Linting cloudes.me

--- src/scripts/content/cloudes.js
+++ src/scripts/content/cloudes.js
@@ -1,16 +1,16 @@
 /*jslint indent: 2 */
-/*global $: false, document: false, togglbutton: false*/
+/*global $: false, document: false, togglbutton: false, createTag: false*/
 /*Created by hamdikavak on 2/13/16.*/
 
 'use strict';
 
-togglbutton.render('#left-panel:not(.toggl)', {}, function (elem) {
+togglbutton.render('#left-panel:not(.toggl)', {}, function () {
   var link, wrap = createTag('div'),
     prevElem = $('#left-panel .ui-grid-a'),
     cloudesDescription = function () {
       return $('title').textContent;
     };
-  
+
   link = togglbutton.createTimerLink({
     className: 'cloudes',
     description: cloudesDescription

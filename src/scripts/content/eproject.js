c915a9e57af2e1bf1a5362e92751a59c17b800a7
IndrekV <indzuliin@gmail.com>
2016-02-22 09:59:05 +0200

    Linting eproject.js

--- src/scripts/content/eproject.js
+++ src/scripts/content/eproject.js
@@ -4,22 +4,22 @@
 
 
 togglbutton.render('.single-tasks .right-side:not(.toggl)', {observe: true}, function (elem) {
-	var link, description,
-		numElem = $('.task-id', elem),
-		titleElem = $('.entry-title', elem),
-		projectElem = $('.project a span.label');
+  var link, description,
+    numElem = $('.task-id', elem),
+    titleElem = $('.entry-title', elem),
+    projectElem = $('.project a span.label');
 
-	description = titleElem.innerText;
-	if (numElem !== null) {
-		description = numElem.innerText + " " + description;
-	}
+  description = titleElem.innerText;
+  if (numElem !== null) {
+    description = numElem.innerText + " " + description;
+  }
 
-	link = togglbutton.createTimerLink({
-		className: 'eproject',
-		description: description,
-		projectName: projectElem.innerText
-	});
+  link = togglbutton.createTimerLink({
+    className: 'eproject',
+    description: description,
+    projectName: projectElem.innerText
+  });
 
-	$('.toggl-timer').appendChild(link);
+  $('.toggl-timer').appendChild(link);
 
 });

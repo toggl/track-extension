bfac65ea3d3d8f89d2cb6fa90a425d7095ecfb09
IndrekV <indzuliin@gmail.com>
2016-02-22 13:47:50 +0200

    Linting gogs.js

--- src/scripts/content/gogs.js
+++ src/scripts/content/gogs.js
@@ -4,18 +4,20 @@
 'use strict';
 
 togglbutton.render('#issue-title:not(.toggl)', {observe: true}, function (elem) {
-  var link, description,
+  var link,
     titleElem = $("#issue-title"),
-    projectElem = $('.issue .header .breadcrumb a:last-child');
-  description = titleElem.textContent;
-  description = description.trim();
+    description = titleElem.textContent.trim(),
+    projectElem = $('.issue .header .breadcrumb a:last-child'),
+    project;
+
   description = $(".index").textContent + " " + description;
-  var project = projectElem.textContent.split(' / ').pop();
+  project = projectElem.textContent.split(' / ').pop();
+
   link = togglbutton.createTimerLink({
-    className: 'gitlab',
+    className: 'gogs',
     description: description,
     projectName: project
   });
-  link.title = project+": "+description;
+
   $('.title h1').appendChild(link);
 });

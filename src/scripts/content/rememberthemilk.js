4594534c399352d4f3284df270f6cc2be234c2d7
IndrekV <indzuliin@gmail.com>
2016-03-08 15:31:16 +0200

    Fixed styles and selectors in remember the milk

--- src/scripts/content/rememberthemilk.js
+++ src/scripts/content/rememberthemilk.js
@@ -3,13 +3,11 @@
 'use strict';
 
 togglbutton.render('.b-eb-Uk-Vj:not(.toggl)', {observe: true}, function (elem) {
-  var link, project,
-    description = $('.b-eb-Uk-Tl', elem).textContent.trim(),
-    tagElem = $('.b-eb-Uk-o', $('.b-eb-Uk-m', elem));
-
-  if (tagElem) {
-    project = tagElem.textContent.trim();
-  }
+  var link,
+    project = function () {
+      return $('.zl-Uk-hl .zl-Uk-xf .zl-Uk-Tl').innerText;
+    },
+    description = $('.b-eb-Uk-Tl', elem).textContent.trim();
 
   link = togglbutton.createTimerLink({
     className: 'rememberthemilk',

996bc35c97ce0c8a2ad11dba00a2ee3376008d64
IndrekV <indzuliin@gmail.com>
2016-03-08 15:07:27 +0200

    Used Observer to add 'Start timer' link into SourceLair

--- src/scripts/content/sourcelair.js
+++ src/scripts/content/sourcelair.js
@@ -2,32 +2,25 @@
 /*global $: false, document: false, togglbutton: false*/
 'use strict';
 
-var sourceLair = {
-  render: function(options) {
-    var self = this;
-    togglbutton.render('#app:not(.toggl)', {observe: true}, function (elem) {
-      var link,
-          project = $('span.project-name.value', elem).textContent,
-          inlineCss = 'position: fixed; bottom: 1rem; right: 1rem; z-index: 9999;',
-          container = document.createElement('div');
+togglbutton.render('#ide:not(.toggl)', {observe: true}, function () {
+  var link,
+    projectFunc = function () {
+      return $('.project-label .project-name').getAttribute('title');
+    },
+    descFunc = function () {
+      return $('.project-label .after.actionable').innerHTML;
+    },
+    inlineCss = 'position: fixed; bottom: 1rem; right: 1rem; z-index: 9999;',
+    container = document.createElement('div');
 
-      container.setAttribute('id', 'toggl-sourceLair');
-      container.setAttribute('style', inlineCss);
+  container.setAttribute('id', 'toggl-sourceLair');
+  container.setAttribute('style', inlineCss);
 
-      link = togglbutton.createTimerLink({
-        projectName: options.projectName || 'Project',
-        description: options.description || 'Task description'
-      });
+  link = togglbutton.createTimerLink({
+    projectName: projectFunc,
+    description: descFunc
+  });
 
-      container.appendChild(link);
-      $('.editor-panel').appendChild(container);
-    });
-  }
-}
-
-// It would be nice if we could listen to an IDE ready event.
-setTimeout(function() {
-  var project = $('.project-label .project-name').getAttribute('title');
-  var description = $('.project-label .after.actionable').innerHTML;
-  sourceLair.render({projectName: project, description: description});
-}, 5000);
+  container.appendChild(link);
+  $('.editor-panel').appendChild(container);
+});

d9a5fe574389077e1b89aecbe2160f86b364d682
IndrekV <indzuliin@gmail.com>
2016-03-09 12:56:20 +0200

    Simplyfied evernote integration

--- src/scripts/content/evernote.js
+++ src/scripts/content/evernote.js
@@ -1,75 +1,30 @@
 /*jslint indent: 2 */
-/*global $: false, document: false, togglbutton: false, createTag: false, MutationObserver: false*/
+/*global $: false, document: false, togglbutton: false, createTag: false*/
 'use strict';
 
-var toggl_container = '#gwt-debug-NoteView-root';
-var toggl_btn_container;
-var toggl_div_container = '#gwt-debug-NoteAttributesView-root > div > div:nth-child(2)';
+togglbutton.render('#gwt-debug-NoteView-root:not(.toggl)', {observe: true}, function (elem) {
+  var link,
+    descFunc,
+    projectFunc,
+    container,
+    parent = document.querySelector('#gwt-debug-NoteAttributesView-root > div > div:nth-child(2)', elem);
 
-var toggl_desc = '#gwt-debug-NoteTitleView-textBox'; // name of note
-//var toggl_proj = '#gwt-debug-NotebookSelectMenu-notebookName'; // name of notebook, doesn't work right now
+  descFunc = function () {
+    return document.querySelector('#gwt-debug-NoteAttributesView-root .GBCTSOIBJE').textContent;
+  };
 
-var toggl_btn = '.toggl-button';
-var toggl_btn_active = toggl_btn + '.active';
+  projectFunc = function () {
+    return document.querySelector('#gwt-debug-NotebookSelectMenu-notebookName').textContent;
+  };
 
-function toggl() {
-  // stop previous tracking (clickEvent)
-  if ($(toggl_btn_active) !== null) {
-    var event = document.createEvent('HTMLEvents');
-    event.initEvent('click', true, false);
-    $(toggl_btn_active).dispatchEvent(event);
-  }
-
-  // delete existing button
-  if ($(toggl_btn) !== null) {
-    $(toggl_div_container).removeChild(toggl_btn_container);
-    $(toggl_container).classList.remove('toggl');
-  }
-
-  // create new button
-  togglbutton.render(toggl_container + ':not(.toggl)', {observe: true}, function (elem) {
-    var link,
-      description = $(toggl_desc, elem).value,
-      //project = $(toggl_proj, elem).textContent,
-      parent = $(toggl_div_container);
-
-    link = togglbutton.createTimerLink({
-      className: 'evernote',
-      description: description,
-      //projectName: project
-    });
-
-    toggl_btn_container = createTag('div', 'toggl-wrapper evernote');
-    toggl_btn_container.appendChild(link);
-
-    parent.insertBefore(toggl_btn_container, parent.firstChild);
+  link = togglbutton.createTimerLink({
+    className: 'evernote',
+    description: descFunc,
+    projectName: projectFunc
   });
-}
 
-// OBSERVATION
-// observe changing of notes
+  container = createTag('div', 'toggl-wrapper evernote');
+  container.appendChild(link);
 
-var targetString = '#gwt-debug-NoteAttributesView-root > div > div:nth-child(3) > div > div:nth-child(2)';
-var target;
-
-function observe() {
-  var observer = new MutationObserver(function () {
-    toggl();
-  }),
-    config = {childList: true};
-  observer.observe(target, config);
-}
-
-// READY
-// start observation when page loaded fully
-
-function ready() {
-  target = $(targetString);
-  if (target !== null) {
-    observe();
-  } else {
-    setTimeout(ready, 1000);
-  }
-}
-
-ready();
+  parent.insertBefore(container, parent.firstChild);
+});

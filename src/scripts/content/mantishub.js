df460e959645131845d5470854490c3a97d60433
IndrekV <indzuliin@gmail.com>
2016-03-28 15:36:24 +0300

    Added classic UI support to MantisHub

--- src/scripts/content/mantishub.js
+++ src/scripts/content/mantishub.js
@@ -15,3 +15,19 @@ togglbutton.render('.page-content .widget-toolbox .pull-left:not(.toggl)', {obse
 
   elem.appendChild(link);
 });
+
+
+// Classic UI
+togglbutton.render('#view-issue-details:not(.toggl)', {observe: true}, function (elem) {
+  var link,
+    description = $('td.bug-summary', elem).textContent,
+    project = $('td.bug-project', elem).textContent;
+
+  link = togglbutton.createTimerLink({
+    className: 'mantishub',
+    description: description,
+    projectName: project
+  });
+
+  $('.form-title', elem).appendChild(link);
+});

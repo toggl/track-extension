7cb790c4902265413ed07d30a9c99db16bf3635e
Szymon Dudzik <szymon.dudzik@ageno.pl>
2016-03-29 16:22:02 +0200

    AgenoCRM and miniCRM integration

---
+++ src/scripts/content/minicrm.js
@@ -0,0 +1,18 @@
+/*jslint indent: 2, unparam: true*/
+/*global $: false, document: false, togglbutton: false*/
+'use strict';
+
+togglbutton.render('.task-header .meta:not(.toggl)', {}, function (elem) {
+
+    var link,
+        description = $('.title h1', elem).textContent.trim(),
+        project = $('[data-contexttype="deal"]', elem).textContent.trim();
+
+    link = togglbutton.createTimerLink({
+        className: 'agenocrm',
+        description: description,
+        projectName: project
+    });
+
+    elem.appendChild(link);
+});

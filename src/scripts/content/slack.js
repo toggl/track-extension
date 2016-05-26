b4e4ddd3f893c9f4430e234bb57cad7f41843766
IndrekV <indzuliin@gmail.com>
2016-05-26 12:45:46 +0300

    Updated Slack integration

--- src/scripts/content/slack.js
+++ src/scripts/content/slack.js
@@ -2,10 +2,10 @@
 /*global $: false, document: false, togglbutton: false, createTag:false*/
 'use strict';
 
-togglbutton.render('.name:not(.toggl)', {observe: true}, function () {
+togglbutton.render('#channel_name:not(.toggl)', {observe: true}, function () {
   var link,
-    placeholder = $('#channel_actions'),
-    description = $('.name').innerText,
+    placeholder = $('.channel_title_info'),
+    description = $("#channel_name").innerText.trim().substr(1),
     project = $('#team_name').innerText;
 
   link = togglbutton.createTimerLink({

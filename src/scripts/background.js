toggl/track-extension

commit c7b611fde6414d34aea41d89f377b5457e3ad4ae

Fixed browser auto start function by adding command queque, closes #407

src/scripts/background.js
@@ -58,6 +58,7 @@ TogglButton = {
   $userState: 'active',
   $fullVersion: ("TogglButton/" + chrome.runtime.getManifest().version),
   $version: (chrome.runtime.getManifest().version),
+  queque: [],
   $editForm: '<div id="toggl-button-edit-form">' +
       '<form autocomplete="off">' +
       '<a class="toggl-button {service} active" href="#">Stop timer</a>' +
@@ -155,6 +156,7 @@ TogglButton = {
               TogglButton.setupSocket();
             }
             TogglButton.updateBugsnag();
+            TogglButton.handelQueQue();
           } else if (!token) {
             apiToken = localStorage.getItem('userToken');
             if (apiToken) {
@@ -168,6 +170,12 @@ TogglButton = {
         }
       }
     });
+  },
+
+  handelQueQue: function () {
+    while(TogglButton.queque) {
+      TogglButton.queque.shift()();
+    }
   },
 
   updateBugsnag: function () {
@@ -1087,7 +1095,7 @@ TogglButton = {
     }
   },
   startAutomatically: function () {
-    if (!TogglButton.$curEntry && Db.get("startAutomatically")) {
+    if (!TogglButton.$curEntry && Db.get("startAutomatically") && !!TogglButton.$user) {
       var lastEntryString, lastEntry;
       lastEntryString = Db.get("latestStoppedEntry");
       if (lastEntryString) {
@@ -1106,6 +1114,7 @@ TogglButton = {
   }
 };
 
+TogglButton.queque.push(TogglButton.startAutomatically);
 TogglButton.toggleRightClickButton(Db.get("showRightClickButton"));
 TogglButton.fetchUser();
 TogglButton.triggerNotification();

commit d14b4d7e1cefe73d1e87ac1f56d8def5f68db030

fix spelling errors
introduced with c7b611fd

src/scripts/background.js
@@ -60,7 +60,7 @@ TogglButton = {
   $userState: 'active',
   $fullVersion: ("TogglButton/" + chrome.runtime.getManifest().version),
   $version: (chrome.runtime.getManifest().version),
-  queque: [],
+  queue: [],
   $editForm: '<div id="toggl-button-edit-form">' +
       '<form autocomplete="off">' +
       '<a class="toggl-button {service} active" href="#">Stop timer</a>' +
@@ -157,7 +157,7 @@ TogglButton = {
               TogglButton.setupSocket();
             }
             TogglButton.updateBugsnag();
-            TogglButton.handelQueQue();
+            TogglButton.handleQueue();
           } else if (!token) {
             apiToken = localStorage.getItem('userToken');
             if (apiToken) {
@@ -186,9 +186,9 @@ TogglButton = {
     });
   },
 
-  handelQueQue: function () {
-    while (!!TogglButton.queque.length) {
-      TogglButton.queque.shift()();
+  handleQueue: function () {
+    while (!!TogglButton.queue.length) {
+      TogglButton.queue.shift()();
     }
   },
 
@@ -1335,7 +1335,7 @@ TogglButton = {
   }
 };
 
-TogglButton.queque.push(TogglButton.startAutomatically);
+TogglButton.queue.push(TogglButton.startAutomatically);
 TogglButton.toggleRightClickButton(Db.get("showRightClickButton"));
 TogglButton.fetchUser();
 TogglButton.triggerNotification();
d8b0c2ae18e7c8d80728e2cb2057b5a13b9cc8e2
Grzegorz Jazurek <grzegorz.jazurek@gmail.com>
2016-07-08 21:18:34 +0200

    #596 mark destination container with toggl class

--- src/scripts/content/visualstudio.js
+++ src/scripts/content/visualstudio.js
@@ -6,7 +6,9 @@
 togglbutton.render('.work-item-form-main-core:not(.toggl)', {observe: true}, function () {
   var link,
     description = document.querySelector('.work-item-form-title input').value,
-    project = document.querySelector('.work-item-form-areaIteration input').value;
+    project = document.querySelector('.work-item-form-areaIteration input').value,
+    destinationContainer = document
+      .querySelector('.work-item-form-header-controls-container:not(.toggl)');
 
   link = togglbutton.createTimerLink({
     className:   'visual-studio-online',
@@ -14,5 +16,8 @@ togglbutton.render('.work-item-form-main-core:not(.toggl)', {observe: true}, fun
     projectName: project
   });
 
-  document.querySelector('.work-item-form-header-controls-container').appendChild(link);
+  if (destinationContainer) {
+    destinationContainer.appendChild(link);
+    destinationContainer.classList.add('toggl');
+  }
 });

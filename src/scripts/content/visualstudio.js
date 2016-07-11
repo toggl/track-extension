0759ff6d89871340a4cefea63dbdd4d707e99656
IndrekV <indzuliin@gmail.com>
2016-07-11 15:14:06 +0300

    Fixed VSO description fetching, closes #582 closes #596

--- src/scripts/content/visualstudio.js
+++ src/scripts/content/visualstudio.js
@@ -3,21 +3,17 @@
 
 'use strict';
 
-togglbutton.render('.work-item-form-main-core:not(.toggl)', {observe: true}, function () {
+togglbutton.render('.witform-layout-content-container:not(.toggl)', {observe: true}, function (elem) {
   var link,
-    description = document.querySelector('.work-item-form-title input').value,
-    project = document.querySelector('.work-item-form-areaIteration input').value,
-    destinationContainer = document
-      .querySelector('.work-item-form-header-controls-container:not(.toggl)');
+    description = $('.work-item-form-title input', elem).value,
+    project = $('.work-item-form-areaIteration input', elem).value,
+    container = $('.work-item-form-header-controls-container', elem);
 
   link = togglbutton.createTimerLink({
-    className:   'visual-studio-online',
+    className: 'visual-studio-online',
     description: description,
     projectName: project
   });
 
-  if (destinationContainer) {
-    destinationContainer.appendChild(link);
-    destinationContainer.classList.add('toggl');
-  }
+  container.appendChild(link);
 });

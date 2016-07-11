1ed7e67ae74c3789a9e5bd5acc01a4d71424f332
IndrekV <indzuliin@gmail.com>
2016-07-11 14:51:49 +0300

    Linting and fixes for Sherpadesk

--- src/scripts/content/sherpadesk.js
+++ src/scripts/content/sherpadesk.js
@@ -2,22 +2,21 @@
 /*global $: false, document: false, togglbutton: false*/
 'use strict';
 
-togglbutton.render('#ctl00_ctl00_PageBody_tktHeader', {}, function (elem) {
+togglbutton.render('#ctl00_ctl00_PageBody_tktHeader', {}, function () {
   var link,
-    description  = '',
-    project = '';   
+    returnPos,
+    description = '',
+    object = $('#ctl00_ctl00_PageBody_lbSubject');
 
-	var object = $('#ctl00_ctl00_PageBody_lbSubject');	
-	description = $('#ctl00_ctl00_PageBody_lbSubject').innerText;
-	var returnPos = description.indexOf('\n');
-	if (returnPos>0) {
-		description = description.substr(0, description.indexOf('\n')); 
-	}
-	
+  description = object.innerText;
+  returnPos = description.indexOf('\n');
+  if (returnPos > 0) {
+    description = description.substr(0, description.indexOf('\n'));
+  }
+
   link = togglbutton.createTimerLink({
     className: 'sherpadesk',
-    description: description,
-	projectName: project
+    description: description
   });
 	
   link.setAttribute('style', 'line-height: 20px;vertical-align: middle;');

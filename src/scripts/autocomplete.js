672adcdd97eeaebffded3a7f65338ceb123553d7
IndrekV <indzuliin@gmail.com>
2016-07-14 08:21:08 +0300

    Removed unused jslint rules from autocomplete

--- src/scripts/autocomplete.js
+++ src/scripts/autocomplete.js
@@ -1,5 +1,5 @@
 /*jslint indent: 2, unparam: true, plusplus: true*/
-/*global document: false, window: false, XMLHttpRequest: false, chrome: false, btoa: false, localStorage:false */
+/*global document: false */
 "use strict";
 
 var inheritsFrom = function (child, parent) {

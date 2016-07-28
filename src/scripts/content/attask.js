b40339066be71a80e96ae0d1482260a14058f4bf
Gonen Shoham (AMR) <gshoham@amresorts.com>
2016-07-28 10:33:07 -0400

    Change format to a more human-friendly version

--- src/scripts/content/attask.js
+++ src/scripts/content/attask.js
@@ -5,34 +5,34 @@
 
 togglbutton.render('#layout-container:not(.toggl)', {observe: true}, function (elem) {
   var link,
-  description,
-  project = $('.objectlink.breadcrumb-link.first-item', elem).textContent;
+  description;
 
   description = function(){
-    var delimiter = ' > ',
-    w_type = window.location.pathname.split('/')[1],
-    w_id = $('#layout-content').innerHTML.split('detailObjID":"')[1].split('"')[0],
+    var w_url = window.location.href,
     w_name = $('.detail-name-edit', elem).textContent;
 
-    return w_type + delimiter + w_name + delimiter + w_id;
+    return w_name + ' ' + w_url;
   };
 
   link = togglbutton.createTimerLink({
     className: 'attask',
-    description: description,
-    projectName: project
+    description: description
   });
 
+  // Create style tag
   var t_style = document.createElement("style");
   t_style.innerHTML = ".toggl-button{height: 62px;line-height: 62px;background-position: 1em 50%;padding: 0 1em 0 2.75em;}#nav-toggl{height: 62px; vertical-align: middle; } .toggl-button:hover{background-color:#eee !important;} .toggl-button.active{background-color:#f5f5f5;}";
   document.head.appendChild(t_style);
 
+  // Create container element
   var t_container = document.createElement("li");
   t_container.id = "nav-toggl";
   t_container.className = "navbar-item";
 
+  // Add Toggl button to container
   t_container.appendChild(link);
 
+  // Add container to navbar
   var navgroup = document.querySelector('.navbar-item-group.right');
   navgroup.insertBefore(t_container, navgroup.children[0]);
 

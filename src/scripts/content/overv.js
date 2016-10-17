353072c86e4bca929d514ea1257ff4305e294889
IndrekV <indzuliin@gmail.com>
2016-10-17 06:20:46 +0300

    Added project name support for overv.io

--- src/scripts/content/overv.js
+++ src/scripts/content/overv.js
@@ -4,15 +4,17 @@
 
 togglbutton.render('.modal-header .content:not(.toggl)',  {observe: true}, function (elem) {
 
-  var link, description;
+  var link,
+    description = $('.modal-header .info .number').textContent + ' ' + $('.modal-header .content .title').textContent,
+    projectName = $('.repo-icon').getAttribute('title').split('/');
 
-  description = $('.modal-header .info .number').textContent + ' ' + $('.modal-header .content .title').textContent;
+  projectName = projectName[projectName.length - 1];
 
   link = togglbutton.createTimerLink({
     className: 'overv-io',
-    description: description
+    description: description,
+    projectName: projectName
   });
-
 
   elem.appendChild(link);
 });

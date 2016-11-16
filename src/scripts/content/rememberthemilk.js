f3d304547b7c52c963ce972bc94d54119df41b6c
IndrekV <indzuliin@gmail.com>
2016-11-16 09:12:13 +0200

    Linting Remember the milk

--- src/scripts/content/rememberthemilk.js
+++ src/scripts/content/rememberthemilk.js
@@ -3,35 +3,34 @@
 'use strict';
 
 togglbutton.render('.b-db-Lm:not(.toggl)', {observe: true}, function (elem) {
-    // Execute main function.
-    injectTogglButton();
+  var getDescription,
+    getProject,
+    createTogglButton;
 
-    // Inject toggl button to each task.
-    function injectTogglButton() {
-        elem.querySelector('.b-db-Lm-xn').appendChild(createTogglButton());
-    }
+  // Get task's description.
+  getDescription = function () {
+    return elem.querySelector('.b-db-Lm-xn > .b-db-Lm-Nj').textContent.trim();
+  };
 
-    // Create and return toggl button's instance.
-    function createTogglButton() {
-        return togglbutton.createTimerLink({
-            className: 'rememberthemilk',
-            description: getDescription,
-            projectName: getProject,
-            buttonType: 'minimal'
-        });
+  // Get project name if in project task view
+  getProject = function () {
+    var p = $('.b-Mj.b-wd .b-f-n');
+    if (!p) {
+      return;
     }
+    return p.textContent;
+  };
 
-    // Get task's description.
-    function getDescription() {
-        return elem.querySelector('.b-db-Lm-xn > .b-db-Lm-Nj').textContent.trim();
-    }
+  // Create and return toggl button's instance.
+  createTogglButton = function () {
+    return togglbutton.createTimerLink({
+      className: 'rememberthemilk',
+      description: getDescription,
+      projectName: getProject,
+      buttonType: 'minimal'
+    });
+  };
 
-    // Get project name if in project task view
-    function getProject() {
-        var p = $('.b-Mj.b-wd .b-f-n');
-        if (!p) {
-            return;
-        }
-        return p.textContent;
-    }
+  // Inject toggl button to each task.
+  elem.querySelector('.b-db-Lm-xn').appendChild(createTogglButton());
 });

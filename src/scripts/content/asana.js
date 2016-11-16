7491487755eb28bfc9467119ecf45fbf0933ac51
IndrekV <indzuliin@gmail.com>
2016-11-16 15:21:54 +0200

    Keep both old and new Asana ui implementation

--- src/scripts/content/asana.js
+++ src/scripts/content/asana.js
@@ -3,6 +3,32 @@
 
 'use strict';
 
+// Older UI
+togglbutton.render('.details-pane-body:not(.toggl)', {observe: true}, function (elem) {
+
+  var link, descFunc, projectFunc,
+    container = $('.sticky-view-placeholder', elem),
+    description = $('#details_property_sheet_title', elem),
+    project = $('#details_pane_project_tokenizer .token_name', elem);
+
+  descFunc = function () {
+    return !!description ? description.value : "";
+  };
+
+  projectFunc = function () {
+    return (project && project.textContent) || ($('.ancestor-projects', elem) && $('.ancestor-projects', elem).textContent) || "";
+  };
+
+  link = togglbutton.createTimerLink({
+    className: 'asana',
+    description: descFunc,
+    projectName: projectFunc
+  });
+
+  container.parentNode.insertBefore(link, container.nextSibling);
+});
+
+// New UI
 togglbutton.render('#right_pane__contents .SingleTaskPane:not(.toggl)', {observe: true}, function (elem) {
 
   var link, descFunc, projectFunc,
@@ -19,7 +45,7 @@ togglbutton.render('#right_pane__contents .SingleTaskPane:not(.toggl)', {observe
   };
 
   link = togglbutton.createTimerLink({
-    className: 'asana',
+    className: 'asana-new',
     description: descFunc,
     projectName: projectFunc
   });

4d7d53f1e885ffc1cfe4b868b5682ca5b857d751
IndrekV <indzuliin@gmail.com>
2016-11-16 10:25:38 +0200

    Removed setTimout from clubhouse content script

--- src/scripts/content/clubhouse.js
+++ src/scripts/content/clubhouse.js
@@ -1,24 +1,31 @@
 /*jslint indent: 2 */
-/*global $: false, document: false, togglbutton: false*/
+/*global $: false, document: false, togglbutton: false, createTag: false*/
 'use strict';
 
-togglbutton.render('.story-container:not(.toggl)', {observe: true}, function (elem) {
+togglbutton.render('.story-state:not(.toggl)', {observe: true}, function (elem) {
   var link, wrap = createTag('div'),
-    description = $('h2.story-name', elem).textContent,
-    project = $('.story-project .value', elem).textContent;
+    element = elem,
+    getDescription,
+    getProject;
+
+  elem = elem.parentNode.parentNode.parentNode;
+
+  getDescription = function () {
+    return $('h2.story-name', elem).textContent;
+  };
+
+  getProject = function () {
+    return $('.story-project .value', elem).textContent;
+  };
 
   link = togglbutton.createTimerLink({
     className: 'clubhouse',
-    description: description,
-    projectName: project
+    description: getDescription,
+    projectName: getProject
   });
 
   wrap.className = 'attribute editable-attribute';
   wrap.appendChild(link);
-  // Wait for container to be rendered
-  setTimeout(function(){
-    var element = $('.story-state');
-    element.parentNode.insertBefore(wrap, element.nextSibling);
-  }, 1000);
 
+  element.parentNode.insertBefore(wrap, element.nextSibling);
 });

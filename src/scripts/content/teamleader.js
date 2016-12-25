46e70b0356cb22b5a372acfcff3fb44664c14e5c
Alexander Wolf <alexander.wolf@baur-wolf.de>
2016-12-25 18:01:33 +0100

    Add teamleader support.

---
+++ src/scripts/content/teamleader.js
@@ -0,0 +1,27 @@
+/*jslint indent: 2 */
+/*global $: false, document: false, togglbutton: false*/
+'use strict';
+
+togglbutton.render('.container:not(.toggl)', {observe: true}, function (elem) {
+  var link, taskTitleElem = $('#title', elem), pageTitleElem = $('.content_t', elem), description;
+
+  description = '';
+
+  if (pageTitleElem !== null) {
+    description += pageTitleElem.textContent.trim();
+  }
+
+  if (taskTitleElem !== null) {
+    if (description.length > 0) {
+      description += ": ";
+    }
+    description += taskTitleElem.textContent.trim();
+  }
+
+  link = togglbutton.createTimerLink({
+    className: 'teamleader',
+    description: description
+  });
+
+  elem.getElementsByClassName('widgettitle')[0].appendChild(link);
+});

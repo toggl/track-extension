8a57ed7ef34968fd85420290cfec1b1520f3b022
Joshua Loya <joshuadloya@gmail.com>
2017-04-25 09:25:09 -0500

    Todoist: Move functions so they are only defined once

--- src/scripts/content/todoist.js
+++ src/scripts/content/todoist.js
@@ -3,10 +3,76 @@
 
 'use strict';
 
+function getProjectNameFromLabel(elem) {
+  var projectLabel = '', projectLabelEle = $('.pname', elem.parentNode.parentNode);
+  if (projectLabelEle) {
+    projectLabel = projectLabelEle.textContent.trim();
+  }
+  return projectLabel;
+}
+
+var levelPattern = /(?:^|\s)indent_([0-9]*?)(?:\s|$)/;
+function getParentEle(sidebarEle) {
+  var curLevel, parentClass, parentCandidate;
+  curLevel = sidebarEle.className.match(levelPattern)[1];
+  parentClass = 'indent_' + (curLevel - 1);
+
+  parentCandidate = sidebarEle;
+  while (parentCandidate.previousElementSibling) {
+    parentCandidate = parentCandidate.previousElementSibling;
+    if (parentCandidate.classList.contains(parentClass)) {
+      break;
+    }
+  }
+  return parentCandidate;
+}
+
+function isTopLevelProject(sidebarEle) {
+  return sidebarEle.classList.contains('indent_1');
+}
+
+function getProjectNameHierarchy(sidebarEle) {
+  var parentProjectEle, projectName;
+  projectName = $('.name', sidebarEle).firstChild.textContent.trim();
+  if (isTopLevelProject(sidebarEle)) {
+    return [projectName];
+  }
+  parentProjectEle = getParentEle(sidebarEle);
+  return [projectName].concat(getProjectNameHierarchy(parentProjectEle));
+}
+
+function getSidebarEle(elem) {
+  var editorInstance, projectId, sidebarRoot, sidebarColorEle, sidebarEle;
+  editorInstance = elem.closest('.project_editor_instance');
+  if (editorInstance) {
+    projectId = editorInstance.getAttribute('data-project-id');
+    sidebarRoot = $('#project_list');
+    sidebarColorEle = $('#project_color_' + projectId, sidebarRoot);
+    if (sidebarColorEle) {
+      sidebarEle = sidebarColorEle.closest('.menu_clickable');
+    }
+  }
+  return sidebarEle;
+}
+
+function getProjectNames(elem) {
+  var projectNames, viewingInbox, sidebarEle;
+  viewingInbox = $('#filter_inbox.current, #filter_team_inbox.current');
+  if (viewingInbox) {
+    projectNames = ['Inbox'];
+  } else {
+    sidebarEle = getSidebarEle(elem);
+    if (sidebarEle) {
+      projectNames = getProjectNameHierarchy(sidebarEle);
+    } else {
+      projectNames = [getProjectNameFromLabel(elem)];
+    }
+  }
+  return projectNames;
+}
+
 togglbutton.render('.task_item .content:not(.toggl)', {observe: true}, function (elem) {
-  var link, descFunc, container = $('.text', elem),
-    getSidebarEle, getProjectNameHierarchy, isTopLevelProject, levelPattern, getParentEle,
-    getProjectNameFromLabel, getProjectNames;
+  var link, descFunc, container = $('.text', elem);
 
   descFunc = function () {
     var clone = container.cloneNode(true),
@@ -35,78 +101,10 @@ togglbutton.render('.task_item .content:not(.toggl)', {observe: true}, function 
     return clone.textContent.trim();
   };
 
-  getSidebarEle = function (elem) {
-    var editorInstance, projectId, sidebarRoot, sidebarColorEle, sidebarEle;
-    editorInstance = elem.closest('.project_editor_instance');
-    if (editorInstance) {
-      projectId = editorInstance.getAttribute('data-project-id');
-      sidebarRoot = $('#project_list');
-      sidebarColorEle = $('#project_color_' + projectId, sidebarRoot);
-      if (sidebarColorEle) {
-        sidebarEle = sidebarColorEle.closest('.menu_clickable');
-      }
-    }
-    return sidebarEle;
-  };
-
-  getProjectNameHierarchy = function (sidebarEle) {
-    var parentProjectEle, projectName;
-    projectName = $('.name', sidebarEle).firstChild.textContent.trim();
-    if (isTopLevelProject(sidebarEle)) {
-      return [projectName];
-    }
-    parentProjectEle = getParentEle(sidebarEle);
-    return [projectName].concat(getProjectNameHierarchy(parentProjectEle));
-  };
-
-  isTopLevelProject = function (sidebarEle) {
-    return sidebarEle.classList.contains('indent_1');
-  };
-
-  levelPattern = /(?:^|\s)indent_([0-9]*?)(?:\s|$)/;
-  getParentEle = function (sidebarEle) {
-    var curLevel, parentClass, parentCandidate;
-    curLevel = sidebarEle.className.match(levelPattern)[1];
-    parentClass = 'indent_' + (curLevel - 1);
-
-    parentCandidate = sidebarEle;
-    while (parentCandidate.previousElementSibling) {
-      parentCandidate = parentCandidate.previousElementSibling;
-      if (parentCandidate.classList.contains(parentClass)) {
-        break;
-      }
-    }
-    return parentCandidate;
-  };
-
-  getProjectNameFromLabel = function (elem) {
-    var projectLabel = '', projectLabelEle = $('.pname', elem.parentNode.parentNode);
-    if (projectLabelEle) {
-      projectLabel = projectLabelEle.textContent.trim();
-    }
-    return projectLabel;
-  };
-
-  getProjectNames = function () {
-    var projectNames, viewingInbox, sidebarEle;
-    viewingInbox = $('#filter_inbox.current, #filter_team_inbox.current');
-    if (viewingInbox) {
-      projectNames = ['Inbox'];
-    } else {
-      sidebarEle = getSidebarEle(elem);
-      if (sidebarEle) {
-        projectNames = getProjectNameHierarchy(sidebarEle);
-      } else {
-        projectNames = [getProjectNameFromLabel(elem)];
-      }
-    }
-    return projectNames;
-  };
-
   link = togglbutton.createTimerLink({
     className: 'todoist',
     description: descFunc(),
-    projectName: getProjectNames()
+    projectName: getProjectNames(elem)
   });
 
   container.insertBefore(link, container.lastChild);

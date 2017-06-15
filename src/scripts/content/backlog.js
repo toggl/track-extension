8bc1918e7ebbfb41aa35402336fc79ea0f173beb
tomcat0090 <miku0090@yahoo.co.jp>
2017-06-15 19:05:44 +0900

    Fix. Possibility that A will fail to fetch.
    'h3#summary' has meny 'span'.

--- src/scripts/content/backlog.js
+++ src/scripts/content/backlog.js
@@ -7,7 +7,7 @@ togglbutton.render('#issueArea:not(.toggl)', {observe: true}, function (elem) {
   var link, container = createTag('span', ''),
     descFunc,
     ticketNumElem = $('.ticket__key .ticket__key-number', elem),
-    titleElem = $('h3#summary span', elem),
+    titleElem = $('h3#summary span.title-group__title-text', elem),
     projectElem = $('.project-header h1 .header-icon-set__name'),
     descriptionElem = $('#summary span');
 

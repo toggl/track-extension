b22c2b3c5211d81ed57e94bcf1d5873237ea8bee
yfer <barabanov.mikle@gmail.com>
2017-06-18 02:13:18 +0300

    fixed jslint errors

--- src/scripts/content/google-calendar.js
+++ src/scripts/content/google-calendar.js
@@ -1,9 +1,8 @@
 /*jslint indent: 2 */
-/*global $: false, document: false, togglbutton: false*/
+/*global $: false, document: false, togglbutton: false, MutationObserver: false, HTMLIFrameElement: false*/
 'use strict';
 
-function insertButton(bubblecontent, description)
-{
+function insertButton(bubblecontent, description) {
   var link = togglbutton.createTimerLink({
     className: 'google-calendar',
     description: description
@@ -13,7 +12,7 @@ function insertButton(bubblecontent, description)
 
 // Detail view
 togglbutton.render('.ep:not(.toggl)', {observe: true}, function (elem) {
-  var link, description, togglButtonElement;
+  var description, togglButtonElement;
 
   togglButtonElement = $('.ep-dpc', elem);
   description = $('.ep-title input', elem).value;
@@ -22,40 +21,40 @@ togglbutton.render('.ep:not(.toggl)', {observe: true}, function (elem) {
 });
 
 // Popup view
-togglbutton.render('.bubblecontent:not(.toggl)', {observe: true}, function (elem) {    
-  var description;
+togglbutton.render('.bubblecontent:not(.toggl)', {observe: true}, function (elem) {
   // Goal view  
-  var goal = $('.title-text', elem);
-  if (goal) description = goal.textContent;
+  var description, goal = $('.title-text', elem), event = $('#mtb', elem);
+  if (goal) {
+    description = goal.textContent;
+  }
   // Event view
-  var event = $('#mtb', elem);
-  if (event) description = event.textContent;
+  if (event) {
+    description = event.textContent;
+  }
 
-  if(description)
-    insertButton(elem, description);    
+  if (description) {
+    insertButton(elem, description);
+  }
 });
 
 // Popup view for Tasks
 // we subscribe here for DOM changes, so we could get tasks IFrames with description info
-var observer = new MutationObserver(function(mutations) {
-  mutations.filter(function(mutation) {
+var observer = new MutationObserver(function (mutations) {
+  mutations.filter(function (mutation) {
     //tasks iframes are only one without id or class
-    var iframeselector = function (node) {                
-      return node instanceof HTMLIFrameElement && node.id.length == 0 && node.className.length == 0; 
-    }
-    var iframe = [...mutation.addedNodes.values()].find(iframeselector);
-    if( iframe )
-    {
-      iframe.onload = function(){     
-        var taskname = $('.b', this.contentDocument);
-
-        //got to .bubblecontent so button styles be the same
-        var bubblecontent = this.parentElement.parentElement.parentElement; 
-        if(bubblecontent.classList.contains("bubblecontent"))
-          insertButton(bubblecontent, taskname.textContent);            
+    var iframe = Array.from(mutation.addedNodes.values()).find(function (node) {
+      return node instanceof HTMLIFrameElement && node.id.length === 0 && node.className.length === 0;
+    });
+    if (iframe) {
+      iframe.onload = function () {
+        var taskname = $('.b', this.contentDocument),
+          bubblecontent = this.parentElement.parentElement.parentElement; //got to .bubblecontent so button styles be the same
+        if (bubblecontent.classList.contains("bubblecontent")) {
+          insertButton(bubblecontent, taskname.textContent);
+        }
       };
     }
-  });    
+  });
 });
 
 observer.observe($('body'), { childList: true, subtree: true });

3977c562fea1f9aac2c5ab07228dd9f4a5f30c86
Andrin Riiet <andrin@toggl.com>
2017-08-30 13:10:38 +0300

    Fix rallydev linting errors and bugs
    Realized after merge that there were linting errors,
    discovered bugs while fixing them.
    
    The button should now rerender itself if any of the relevant fields
    change in rallydev.
    
    related to #861

--- src/scripts/content/rallydev.js
+++ src/scripts/content/rallydev.js
@@ -2,26 +2,26 @@
 /*global $: false, document: false, togglbutton: false*/
 'use strict';
 
-togglbutton.render('.chr-EditorsWorkItemEditor-linkSpan:not(.toggl)', {observe: true}, function (elem) {
-  var project, link, div, existingContainer, 
-    description = $('.smb-TextInput-renderedText').textContent, 
-    title = $('.chr-QuickDetailFormattedId'),
-    project = $('.chr-EditorsWorkItemEditor-linkSpan').textContent;
-  existingContainer = $('.togglContainer')
+togglbutton.render('.chr-QuickDetailQuickDetailEditor', {observe: true}, function (elem) {
+  var link, div,
+    descriptionElem = $('.chr-QuickDetailAttributeEditorWrapper--name .smb-TextInput-renderedText', elem),
+    titleElem = $('.chr-QuickDetailFormattedId', elem),
+    projectElem = $('.chr-EditorsWorkItemEditor-linkSpan', elem),
+    existingContainer = $('.togglContainer');
 
   //if page is not refreshed by F5/CTRL + F5 we need to remove existing timer container.
-  if(existingContainer) {
+  if (existingContainer) {
     existingContainer.remove();
   }
 
   link = togglbutton.createTimerLink({
     className: 'rallydev',
-    description: description,
-    projectName: project
+    description: descriptionElem ? descriptionElem.textContent : '',
+    projectName: projectElem ? projectElem.textContent : ''
   });
 
   div = document.createElement("div");
   div.classList.add("timer__container", "togglContainer");
   div.appendChild(link);
-  title.appendChild(div);
-});
+  titleElem.appendChild(div);
+}, '.chr-QuickDetailGridView, .chr-QuickDetailAttributeEditorWrapper--name .smb-TextInput, .smb-TextInput-renderedText, .chr-EditorsWorkItemEditor-linkSpan');

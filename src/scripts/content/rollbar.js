f6c9e5200c84fe3c59930f3b3529228ebe9a5a39
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2017-09-02 02:00:29 +0600

    Add Rollbar integration

---
+++ src/scripts/content/rollbar.js
@@ -0,0 +1,17 @@
+/*jslint indent: 2 */
+/*global $: false, document: false, togglbutton: false*/
+'use strict';
+
+togglbutton.render('#item-title-control:not(.toggl)', {observe: true}, function () {
+  var link,
+    description = $('.item-detail-page-header__item-title').textContent.trim(),
+    project = $('.nav .dropdown-menu .active').textContent.trim();
+
+  link = togglbutton.createTimerLink({
+    className: 'rollbar',
+    description: description,
+    projectName: project
+  });
+
+  $('.item-status-level-area').appendChild(link);
+});

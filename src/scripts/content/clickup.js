79777f0e1dd996d5529c5a34e36598c1630cf8b1
sjsnider <sjsnider@gmail.com>
2017-10-11 11:14:57 -0700

    add integration for ClickUp

---
+++ src/scripts/content/clickup.js
@@ -0,0 +1,19 @@
+/*jslint indent: 2, unparam: true*/
+/*global $: false, document: false, togglbutton: false, createTag: false*/
+'use strict';
+
+togglbutton.render('#timeTrackingItem:not(.toggl)', {observe: true}, function (elem) {
+
+  var link,
+    description = $('.task-name', elem).textContent,
+    project = $('.space-name', elem).textContent;
+
+  link = togglbutton.createTimerLink({
+    className: 'clickup',
+    description: description,
+    projectName: project,
+    buttonType: 'minimal'
+  });
+
+  $('.toggl-container').appendChild(link);
+});

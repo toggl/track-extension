77356fa779798ccdd303d3cfd0cb5fb7de1a449d
thiago <thiagoduarte@gmail.com>
2018-02-08 19:24:09 -0200

    Restore old JIRA integration

--- src/scripts/content/atlassian.js
+++ src/scripts/content/atlassian.js
@@ -3,7 +3,7 @@
 
 'use strict';
 
-// Jira board sidebar
+// Jira 2017 board sidebar
 togglbutton.render('#ghx-detail-view [spacing] h1:not(.toggl)', {observe: true}, function () {
   var link, description,
     rootElem = $('#ghx-detail-view'),
@@ -18,7 +18,7 @@ togglbutton.render('#ghx-detail-view [spacing] h1:not(.toggl)', {observe: true},
   }
 
   link = togglbutton.createTimerLink({
-    className: 'jira',
+    className: 'jira2017',
     description: description,
     buttonType: 'minimal',
     projectName: projectElem && projectElem.textContent
@@ -28,7 +28,7 @@ togglbutton.render('#ghx-detail-view [spacing] h1:not(.toggl)', {observe: true},
   numElem.parentNode.appendChild(container);
 });
 
-// Jira sprint modal
+// Jira 2017 sprint modal
 togglbutton.render('#ghx-issue-fragment [spacing] a img:not(.toggl)', {observe: true}, function () {
   var link, description,
     rootElem = $('#ghx-issue-fragment'),
@@ -43,7 +43,7 @@ togglbutton.render('#ghx-issue-fragment [spacing] a img:not(.toggl)', {observe: 
   }
 
   link = togglbutton.createTimerLink({
-    className: 'jira',
+    className: 'jira2017',
     description: description,
     buttonType: 'minimal',
     projectName: projectElem && projectElem.textContent
@@ -53,7 +53,7 @@ togglbutton.render('#ghx-issue-fragment [spacing] a img:not(.toggl)', {observe: 
   numElem.parentNode.appendChild(container);
 });
 
-// Jira issue page
+// Jira 2017 issue page
 togglbutton.render('.issue-header-content:not(.toggl)', {observe: true}, function (elem) {
   var link, description,
     numElem = $('#key-val', elem),
@@ -72,7 +72,7 @@ togglbutton.render('.issue-header-content:not(.toggl)', {observe: true}, functio
   }
 
   link = togglbutton.createTimerLink({
-    className: 'jira',
+    className: 'jira2017',
     description: description,
     projectName: projectElem && projectElem.textContent
   });
@@ -80,6 +80,59 @@ togglbutton.render('.issue-header-content:not(.toggl)', {observe: true}, functio
   $('.issue-link').parentElement.appendChild(link);
 });
 
+// Jira pre-2017
+togglbutton.render('#ghx-detail-issue:not(.toggl)', {observe: true}, function (elem) {
+  var link, description,
+    container = createTag('div', 'ghx-toggl-button'),
+    titleElem = $('[data-field-id="summary"]', elem),
+    numElem = $('.ghx-fieldname-issuekey a'),
+    projectElem = $('.ghx-project', elem);
+
+  description = titleElem.textContent;
+  if (numElem !== null) {
+    description = numElem.textContent + " " + description;
+  }
+
+  link = togglbutton.createTimerLink({
+    className: 'jira',
+    description: description,
+    projectName: projectElem && projectElem.textContent
+  });
+
+  container.appendChild(link);
+  $('#ghx-detail-head').appendChild(container);
+});
+
+// Jira pre-2017
+togglbutton.render('.issue-header-content:not(.toggl)', {observe: true}, function (elem) {
+  var link, description, ul, li,
+    numElem = $('#key-val', elem),
+    titleElem = $('#summary-val', elem) || "",
+    projectElem = $('#project-name-val', elem);
+
+  if (!!titleElem) {
+    description = titleElem.textContent;
+  }
+
+  if (numElem !== null) {
+    if (!!description) {
+      description = " " + description;
+    }
+    description = numElem.textContent + description;
+  }
+
+  link = togglbutton.createTimerLink({
+    className: 'jira',
+    description: description,
+    projectName: projectElem && projectElem.textContent
+  });
+
+  ul = createTag('ul', 'toolbar-group');
+  li = createTag('li', 'toolbar-item');
+  li.appendChild(link);
+  ul.appendChild(li);
+  $('.toolbar-split-left').appendChild(ul);
+});
 
 //Confluence
 togglbutton.render('#title-heading:not(.toggl)', {observe: true}, function (elem) {

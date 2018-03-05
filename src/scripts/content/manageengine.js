d0e6a71efdf612ab7fadcf677015373c3d69bf4e
Doga Bendasan <doga.bendasan@ebiquity.com>
2018-03-05 10:29:26 +0000

    adding new interface compatibility

--- src/scripts/content/manageengine.js
+++ src/scripts/content/manageengine.js
@@ -3,6 +3,7 @@
 
 'use strict';
 
+//On premise installation
 togglbutton.render('.requestEditbrsty:not(.toggl)', {observe: true}, function (elem) {
   var link,
     description = $('#requestSubject_ID', elem).textContent,
@@ -10,7 +11,6 @@ togglbutton.render('.requestEditbrsty:not(.toggl)', {observe: true}, function (e
     ticketId = $('#requestId', elem).textContent,
     togglCell = document.createElement("td");
 
-    
   link = togglbutton.createTimerLink({
     className: 'manageengine',
     description: ticketId + ' : ' + description,
@@ -20,4 +20,23 @@ togglbutton.render('.requestEditbrsty:not(.toggl)', {observe: true}, function (e
   togglCell.appendChild(link);
 
   $('td#startListMenuItems > table > tbody > tr').appendChild(togglCell);
+});
+
+//Cloud version
+togglbutton.render('#WorkOrderDetailsTable_CT:not(.toggl)', {observe: true}, function (elem) {
+  var link,
+    description = $('#details-middle-container h1', elem).textContent,
+    project = "Tickets to be Allocated",
+    ticketId = $('#reqid', elem).textContent,
+    togglCell = document.createElement("li");
+        
+  link = togglbutton.createTimerLink({
+    className: 'manageengine',
+    description: ticketId + ' : ' + description,
+    projectName: project
+  });
+
+  togglCell.appendChild(link);
+     
+  $('#details-middle-container ul.reply-actions').appendChild(togglCell);
 });

8d36334f88487ff85c0ee96ac2e85423f3400605
thiago <thiagoduarte@gmail.com>
2018-03-08 23:48:53 -0300

    ManageEngine: auto select time entry project
    Try to use the selected project in the issue as the time entry project.

--- src/scripts/content/manageengine.js
+++ src/scripts/content/manageengine.js
@@ -26,17 +26,18 @@ togglbutton.render('.requestEditbrsty:not(.toggl)', {observe: true}, function (e
 togglbutton.render('#WorkOrderDetailsTable_CT:not(.toggl)', {observe: true}, function (elem) {
   var link,
     description = $('#details-middle-container h1', elem).textContent,
-    project = "Tickets to be Allocated",
+    projectElem = $("#projectholder p") || {},
+    project = projectElem.textContent,
     ticketId = $('#reqid', elem).textContent,
     togglCell = document.createElement("li");
-        
+
   link = togglbutton.createTimerLink({
     className: 'manageengine',
-    description: ticketId + ' : ' + description,
+    description: ticketId + ': ' + description,
     projectName: project
   });
 
   togglCell.appendChild(link);
-     
+
   $('#details-middle-container ul.reply-actions').appendChild(togglCell);
 });

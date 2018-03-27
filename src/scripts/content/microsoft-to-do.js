687a6cccb6427c8a736753318260a343283b30a5
Tsvetelin Pavlov <tsvetelin@live.com>
2018-03-28 00:15:00 +0300

    Microsoft To-Do support.

---
+++ src/scripts/content/microsoft-to-do.js
@@ -0,0 +1,29 @@
+/*jslint indent: 2 */
+/*global $: false, document: false, togglbutton: false, createTag:false*/
+
+'use strict';
+
+togglbutton.render('.taskItem-body:not(.toggl)', { observe: true }, function(elem) {
+  var
+      link,
+      container = createTag('a', 'taskItem-toggl'),
+
+      titleElem = $('.taskItem-title', elem),
+      projectTitleElem = $('.taskItemInfo-title', elem),
+      
+      activeList = $('ul.lists .listItem.active'),
+      activeListTitle = $('.listItem-title', activeList);
+
+  var projectTitle = 'empty';
+  projectTitle = projectTitleElem ? projectTitleElem.textContent : activeListTitle ? activeListTitle.textContent : '';
+
+  link = togglbutton.createTimerLink({
+      className: 'microsofttodo',
+      buttonType: 'minimal',
+      description: titleElem.textContent,
+      projectName: projectTitle
+  });
+
+  container.appendChild(link);
+  elem.appendChild(container);
+});

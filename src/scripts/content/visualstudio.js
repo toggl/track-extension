01a96013d5bc07afbbef05b7585deccfe245250a
Mirko Bellabarba <mirko.bellabarba@gmail.com>
2018-03-27 20:13:20 +0200

    Add work item ID in Toggl description (#998)
    * Add work item ID in Toggl description
    
    It is very useful to have the id of the work item in the toggl description.
    In this way, when we start the timer, the description will have this format: "[xxxyyy] - workitem title"
    
    * Modified description template
    
    As requested, I've changed the description template from `[id] - title` to `id title`

--- src/scripts/content/visualstudio.js
+++ src/scripts/content/visualstudio.js
@@ -5,7 +5,7 @@
 // Individual Work Item & Backlog page
 togglbutton.render('.witform-layout-content-container:not(.toggl)', {observe: true}, function () {
   var link,
-    description = $('.work-item-form-title input').value,
+    description = $('.work-item-form-id span').innerText + ' ' + $('.work-item-form-title input').value,
     project = $('.menu-item.l1-navigation-text.drop-visible .text').textContent.trim(),
     container = $('.work-item-form-header-controls-container'),
     vs_activeClassContent = $('.hub-list .menu-item.currently-selected').textContent.trim();

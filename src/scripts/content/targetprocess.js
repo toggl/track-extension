8abeff7e1dea958a7baa548ea7332324b2fc3571
Stéphane Houtteman (Vox) <sho@voxteneo.com>
2018-04-18 00:38:17 +0200

    Fix targetProcess new gui version + add some crash protection

--- src/scripts/content/targetprocess.js
+++ src/scripts/content/targetprocess.js
@@ -6,19 +6,28 @@
 togglbutton.render('div.general-info:not(.toggl)', {observe: true}, function (elem) {
   var link,
     titleElement = $('.i-role-title', elem),
-    entityIdName = $('.entity-id a', elem).textContent + ' ' + titleElement.textContent,
+    entityIdElement = $('.entity-id a', elem),
     projectFunc = function () {
       var projectItem =  $('.tau-linkentity');
       return projectItem ? projectItem.textContent : "";
     };
+	
+	//if element id not found with old tp version, try without the a cause it's just a span in new TP
+	if(entityIdElement== undefined || entityIdElement== null){
+		entityIdElement = $('.entity-id', elem);
+	}
+	
+	//if element id found continue
+	if(entityIdElement!= undefined){
+		var entityIdName = entityIdElement.textContent + ' ' + titleElement.textContent;
+	  link = togglbutton.createTimerLink({
+		className:   'targetprocess',
+		description: entityIdName,
+		projectName: projectFunc
+	  });
 
-  link = togglbutton.createTimerLink({
-    className:   'targetprocess',
-    description: entityIdName,
-    projectName: projectFunc
-  });
-
-  titleElement.parentElement.appendChild(link);
+	  titleElement.parentElement.appendChild(link);
+	}
 });
 
 // entity's task buttons
@@ -47,20 +56,30 @@ togglbutton.render('.tau-list__table__row:not(.toggl)', {observe: true}, functio
 togglbutton.render('.tau-list-line:not(.toggl)', {observe: true}, function (elem) {
   var link,
     buttonPlaceholder,
-    taskId      = '#' + $('.tau-list-general_entity_id-cell', elem).textContent.trim(),
-    taskTitle   = $('.tau-list-entity_name_1line-cell', elem).textContent.trim(),
+	taskIdElement = $('.tau-list-general_entity_id-cell', elem),
+	taskTitleElement = $('.tau-list-entity_name_1line-cell', elem),
     projectFunc = function () {
       var projectItem =  $('.tau-list-project_abbr-unit', elem);
       return projectItem ? projectItem.title : "";
     };
 
-  link = togglbutton.createTimerLink({
-    className:   'targetprocess',
-    description: taskId + ' ' + taskTitle,
-    projectName: projectFunc,
-    buttonType:  'minimal'
-  });
+	//if element id found continue
+	if(taskIdElement!= undefined){
+		var taskId      = '#' + $('.tau-list-general_entity_id-cell', elem).textContent.trim();
+		var taskTitle = '';
+		
+		if(taskTitleElement!= undefined){
+			taskTitle   = ' ' + $('.tau-list-entity_name_1line-cell', elem).textContent.trim();
+		}
+		
+	  link = togglbutton.createTimerLink({
+		className:   'targetprocess',
+		description: taskId + taskTitle,
+		projectName: projectFunc,
+		buttonType:  'minimal'
+	  });
 
-  buttonPlaceholder = $('.tau-board-unit_type_entity-name', elem);
-  buttonPlaceholder.insertBefore(link, buttonPlaceholder.firstChild);
+	  buttonPlaceholder = $('.tau-board-unit_type_entity-name', elem);
+	  buttonPlaceholder.insertBefore(link, buttonPlaceholder.firstChild);
+	}
 });

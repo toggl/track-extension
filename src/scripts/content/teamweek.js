dc39487c667ec2562ac0cd69e5227f35fc6a8998
Shantanu Raj <s@sraj.me>
2018-06-01 18:16:44 +0530

    Update selector for tw closes #1046

--- src/scripts/content/teamweek.js
+++ src/scripts/content/teamweek.js
@@ -3,7 +3,7 @@
 
 'use strict';
 
-togglbutton.render('.timeline-task-popup:not(.toggl)', {observe: true}, function (element) {
+togglbutton.render('.timeline-form:not(.toggl)', {observe: true}, function (element) {
   var titleElement = $('[data-hook=input-name]', element),
     projectElement = $('[name=project_name]', element),
     container = $('[data-hook=row-actions]', element),

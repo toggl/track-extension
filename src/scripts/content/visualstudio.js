af38d84173285c9ee1203a4cb468a068ae15e2d7
Jimmy Lu <HanQuang@outlook.com>
2018-06-05 17:01:20 +0200

    Change VSTS integration for markup change

--- src/scripts/content/visualstudio.js
+++ src/scripts/content/visualstudio.js
@@ -6,9 +6,9 @@
 togglbutton.render('.witform-layout-content-container:not(.toggl)', {observe: true}, function () {
   var link,
     description = $('.work-item-form-id span').innerText + ' ' + $('.work-item-form-title input').value,
-    project = $('.menu-item.l1-navigation-text.drop-visible .text').textContent.trim(),
+    project = $('.tfs-selector span').innerText,
     container = $('.work-item-form-header-controls-container'),
-    vs_activeClassContent = $('.hub-list .menu-item.currently-selected').textContent.trim();
+    vs_activeClassContent = $('.commandbar.header-bottom > .commandbar-item > .displayed').innerText;
 
   link = togglbutton.createTimerLink({
     className: 'visual-studio-online',
@@ -16,7 +16,7 @@ togglbutton.render('.witform-layout-content-container:not(.toggl)', {observe: tr
     projectName: project
   });
 
-  if (vs_activeClassContent === "Work Items*" || vs_activeClassContent === "Backlogs") {
+  if (vs_activeClassContent === "Work Items" || vs_activeClassContent === "Backlogs") {
     container.appendChild(link);
   }
 });

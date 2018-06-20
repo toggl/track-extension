1d9d0d4ac8705c2a882d06ce236b5ffdf5b63300
Rudi von Staden <rudivs@gmail.com>
2018-06-20 16:29:22 +0200

    Improves project identification, adds tag support for Workast

--- src/scripts/content/workast.js
+++ src/scripts/content/workast.js
@@ -2,22 +2,49 @@
 /*global $: false, document: false, togglbutton: false, createTag: false*/
 'use strict';
 
+function toProperCase(input) {
+    return input.replace(/\w\S*/g, function(str){
+        return str.charAt(0).toUpperCase() + str.substr(1).toLowerCase();
+    });
+}
+
 /* Checklist buttons */
 togglbutton.render('a.task-list__item:not(.toggl)', {observe: true}, function (elem) {
   var link,
-    titleElem = $('.item__title',elem),
-    titleContainer = $('.layout-column',elem),
-    projectContainer = $('.task-list__title .ng-binding',elem.closest('.task-list__container'));
-  
-  if (!projectContainer) {
+    title = $('.item__title',elem).textContent.trim(),
+    titleContainer = $('.layout-column',elem);
+
+  if (!titleContainer) {
     return;
   }
+
+  // If the current list header is "Open tasks" get the project from the dashboard (current space) title
+  var subProject = $('.task-list__title .ng-binding',elem.closest('.task-list__container')).textContent.trim(),
+    project = (subProject === 'Open tasks' ? $('.dashboard__title').textContent : subProject);
+
+  var tagFunc = function () {
+    var tags = [];
+    var isDetailedView = (elem.getElementsByClassName('item__meta--minimize').length > 0);
+    if (isDetailedView) {
+      var elements = (elem.nextElementSibling.getElementsByClassName('item-name'));
+      for(var i=0;typeof(elements[i])!='undefined';tags.push(elements[i++].textContent));
+    } else {
+      var elements = (elem.getElementsByClassName('item__meta--tag'));
+      for(var i=0;typeof(elements[i])!='undefined';tags.push(elements[i++].getAttribute('aria-label')));
+    }
+    return tags;
+  };
+
+  // Slack channels have a leading #, do not allow spaces, and are all lowercase.
+  // Reformat to match typical project names
+  project = project.charAt(0) === '#'? toProperCase(project.replace(/^#/, '').replace(/[-_]/g, ' ')) : project;
 
   link = togglbutton.createTimerLink({
     className: 'workast',
     buttonType: 'minimal',
-    description: titleElem.textContent.trim(),
-    projectName: projectContainer.textContent.trim()
+    description: title,
+    projectName: project,
+    tags: tagFunc
   });
 
   titleContainer.parentNode.insertBefore(link, titleContainer);

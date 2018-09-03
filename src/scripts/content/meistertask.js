e7377a21cfb8d007c16bdff7ca6b4ce4c27772d2
Maciek Ryłko <rylko.maciej@gmail.com>
2018-09-03 09:12:10 +0200

    Fix for Meistertask 2.0 crash when project does not exist
    close #1106

--- src/scripts/content/meistertask.js
+++ src/scripts/content/meistertask.js
@@ -44,10 +44,12 @@ togglbutton.render('.js-box-wrapper:not(.toggl)', { observe: true }, function(
 // MeisterTask 2.0 (2018)
 // since they removed all descriptive classes selectors looks awful
 togglbutton.render('.kr-view.react-dialog-box > .kr-view:not(.toggl)', { observe: true }, function (elem) {
-  var link, description, project, tagFunc, togglButtonElement;
+  var link, description, projectElem, project, tagFunc, togglButtonElement;
 
   description = $('div.react-modals-enable-events > div > div > div:nth-child(1) > div > div > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div > div', elem).textContent;
-  project = $('div.react-modals-enable-events > div > div > div:nth-child(1) > div > div > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(4) > div:nth-child(1) > a', elem).textContent;
+  projectElem = $('div.react-modals-enable-events > div > div > div:nth-child(1) > div > div > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(4) > div:nth-child(1) > a', elem);
+
+  project = projectElem ? projectElem.textContent : ''
 
   tagFunc = function () {
     var index,

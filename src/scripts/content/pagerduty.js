fdf540096648d0826f1b03ebbe94a0326a3edc90
AlejoNext <alejonext@gmail.com>
2018-09-19 13:56:31 -0500

    Pagerduty update

--- src/scripts/content/pagerduty.js
+++ src/scripts/content/pagerduty.js
@@ -23,3 +23,29 @@ togglbutton.render(
     $('.pd-incident-actions').appendChild(link);
   }
 );
+
+// New interface 09-2018
+togglbutton.render(
+  '.pd-page.pd-page--incidents:not(.toggl)',
+  { observe: true },
+  function(mod) {
+    /*jslint browser:true */
+    var link,
+      description = document.title.replace(
+        /\[#(\d*)\]([\w\W]*) - PagerDuty/g,
+        'Pagerduty $1:$2'
+      );
+      projectElem = $('.pd-service-name__name a');
+
+    if(description) {
+      link = togglbutton.createTimerLink({
+        className: 'pagerduty-09-2018',
+        description: description,
+        projectName: projectElem && projectElem.textContent
+      });
+
+      $('.pd-action-bar__left-items').appendChild(link);
+      mod.classList.add('toggl');
+    }
+  }
+);

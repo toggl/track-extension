ea294d42bce8a3f654ff9f8af876192e32db05b9
Filip Dupanović <filip.dupanovic@gmail.com>
2018-10-09 22:18:18 +0200

    Update Bugsnag client settings
    Closes #1144.

--- src/scripts/lib/bugsnag.js
+++ src/scripts/lib/bugsnag.js
@@ -2,7 +2,12 @@ import bugsnag from 'bugsnag-js';
 
 const client = bugsnag({
   apiKey: process.env.BUGSNAG_API_KEY,
-  appVersion: process.env.APP_VERSION
+  appVersion: process.env.APP_VERSION,
+  releaseStage: process.env.NODE_ENV,
+  notifyReleaseStages: ['production', 'development'],
+  autoBreadcrumbs: false,
+  autoCaptureSessions: false,
+  collectUserIp: false
 });
 
 client.beforeNotify = function(error, metaData) {

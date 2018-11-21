b8f84304e14a1fe5c1d85ed170022b0193bec3e2
Tyler Crammond <tylerchc92@gmail.com>
2018-11-21 09:36:26 +0000

    Fix backlog integration
    Fixes #1181.

--- src/scripts/content/backlog.js
+++ src/scripts/content/backlog.js
@@ -5,13 +5,9 @@ togglbutton.render('#issueArea:not(.toggl)', { observe: true }, function(elem) {
     container = createTag('span', ''),
     descFunc,
     ticketNumElem = $('.ticket__key .ticket__key-number', elem),
-    titleElem = $('h3#summary span.title-group__title-text', elem),
-    projectElem = $('.project-header h1 .header-icon-set__name'),
-    descriptionElem = $('#summary span');
-
-  if (!descriptionElem) {
-    return;
-  }
+    titleElem = $('#summary .title-group__title-text', elem),
+    projectElem = $('.project-header .header-icon-set__name'),
+    containerElem = $('#summary *:first-child');
 
   descFunc = function() {
     return ticketNumElem.textContent + ' ' + titleElem.textContent;
@@ -25,5 +21,5 @@ togglbutton.render('#issueArea:not(.toggl)', { observe: true }, function(elem) {
   });
 
   container.appendChild(link);
-  descriptionElem.parentNode.appendChild(container, descriptionElem);
+  containerElem.parentNode.appendChild(container, containerElem);
 });

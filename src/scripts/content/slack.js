02961269669b0a34a0f450c03b680eb35d03be7f
Tyler Crammond <tylerchc92@gmail.com>
2018-12-11 13:01:11 +0000

    Lint/prettify Slack integration

--- src/scripts/content/slack.js
+++ src/scripts/content/slack.js
@@ -1,36 +1,33 @@
 'use strict';
+/* global togglbutton, $ */
 
-togglbutton.render('#channel_name:not(.toggl)', { observe: true }, function() {
-  var link,
-    placeholder = $('.channel_title_info'),
-    project = $('#team_name').textContent,
-    description = $('#channel_name')
-      .textContent.trim()
-      .replace(/^#/, '');
+togglbutton.render('#channel_name:not(.toggl)', { observe: true }, () => {
+  const placeholder = $('.channel_title_info');
+  const projectName = $('#team_name').textContent.trim();
+  const description = $('#channel_name')
+    .textContent.trim()
+    .replace(/^#/, '');
 
-  link = togglbutton.createTimerLink({
+  const link = togglbutton.createTimerLink({
     className: 'slack',
-    description: description,
-    projectName: project,
+    description,
+    projectName,
     buttonType: 'minimal'
   });
 
   placeholder.parentNode.insertBefore(link, placeholder);
 });
 
-togglbutton.render('.c-message--hover:not(.toggl)', { observe: true }, function(
-  elem
-) {
-  var link,
-    placeholder = $('.c-message_actions__button:last-child'),
-    description = $('.c-message__body', elem).textContent,
-    project = $('#team_name').textContent,
-    button = document.createElement('button');
+togglbutton.render('.c-message--hover:not(.toggl)', { observe: true }, elem => {
+  const placeholder = $('.c-message_actions__button:last-child');
+  const description = $('.c-message__body', elem).textContent.trim();
+  const projectName = $('#team_name').textContent.trim();
+  const button = document.createElement('button');
 
-  link = togglbutton.createTimerLink({
+  const link = togglbutton.createTimerLink({
     className: 'slack-message',
-    projectName: project,
-    description: description,
+    projectName,
+    description,
     buttonType: 'minimal'
   });
 

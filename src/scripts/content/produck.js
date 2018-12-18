aa55c43dddd457feab87f13a5e9350947feede86
Tyler Crammond <tylerchc92@gmail.com>
2018-12-18 15:58:16 +0000

    Lint src/scripts/content/produck.js to new standard

--- src/scripts/content/produck.js
+++ src/scripts/content/produck.js
@@ -1,36 +1,41 @@
-/*jslint indent: 2 */
-/*global $: false, document: false, togglbutton: false*/
+/* global $, togglbutton */
 'use strict';
 
 // Issue details view
-togglbutton.render('[data-toggl-issue] [data-toggl-sidebar]:not(.toggl)', {observe: true}, function (elem) {
-  var li, link, getDescription, getProjectName, getAlias, getTitle;
+togglbutton.render(
+  '[data-toggl-issue] [data-toggl-sidebar]:not(.toggl)',
+  { observe: true },
+  elem => {
+    const getAlias = function() {
+      const aliasSelector = $('[data-toggl-issue] [data-toggl-alias]');
+      if (!aliasSelector) return 'No Alias';
+      return aliasSelector.getAttribute('data-toggl-alias');
+    };
 
-  getAlias = function() {
-    var aliasSelector = $('[data-toggl-issue] [data-toggl-alias]');
-    if(!aliasSelector) return 'No Alias';
-    return aliasSelector.getAttribute('data-toggl-alias');
-  }
+    const getTitle = function() {
+      const titleSelector = $('[data-toggl-issue] [data-toggl-title]');
+      if (!titleSelector) return 'No Title';
+      return titleSelector.getAttribute('data-toggl-title') || 'No Title';
+    };
 
-  getTitle = function() {
-    var titleSelector = $('[data-toggl-issue] [data-toggl-title]');
-    if(!titleSelector) return 'No Title';
-    return titleSelector.getAttribute('data-toggl-title') || 'No Title';
-  }
+    const getDescription = function() {
+      return getAlias() + ' - ' + getTitle();
+    };
 
-  getDescription = function() {
-    return getAlias() + " - " + getTitle();
-  }
+    const getProjectName = function() {
+      const projectSelector = $('[data-toggl-issue] [data-toggl-project]');
+      if (!projectSelector) return null;
+      return projectSelector.getAttribute('data-toggl-project');
+    };
 
-  getProjectName = function() {
-    var projectSelector = $('[data-toggl-issue] [data-toggl-project]');
-    if(!projectSelector) return null;
-    return projectSelector.getAttribute('data-toggl-project');
-  }
-
-    link = togglbutton.createTimerLink({className: 'produck', description: getDescription, projectName: getProjectName});
-    li = document.createElement("li");
-    li.classList.add("toggl-item");
+    const link = togglbutton.createTimerLink({
+      className: 'produck',
+      description: getDescription,
+      projectName: getProjectName
+    });
+    const li = document.createElement('li');
+    li.classList.add('toggl-item');
     li.appendChild(link);
     elem.prepend(li);
-});
+  }
+);

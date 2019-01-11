000b7fd7355c4eded5060836d60d733d54531893
Artem Goncharuk <artexoid@gmail.com>
2019-01-11 17:02:06 +0300

    Removed local transition from style

--- src/scripts/content/turtle.js
+++ src/scripts/content/turtle.js
@@ -48,7 +48,6 @@ togglbutton.render('.card-list-wrapper a.card', { observe: true }, (element) => 
     buttonType: 'minimal'
   });
 
-
   link.addEventListener('mousedown', event => {
     event.preventDefault();
     event.stopImmediatePropagation();

0b60bdcd072049ac1ce934dd048df3059f959c29
Tyler Crammond <tylerchc92@gmail.com>
2019-02-04 13:04:51 +0000

    Add basic support for Lightning Console views
    Related #734.
    
    Remove redundant Salesforce integration code
    
    Remove lightning suffixes from Salesforce class names

--- src/scripts/content/salesforce.js
+++ src/scripts/content/salesforce.js
@@ -1,95 +1,5 @@
 'use strict';
 
-// Updated Listing view
-togglbutton.render(
-  '.bMyTask .list tr.dataRow:not(.toggl)',
-  { observe: true },
-  function (elem) {
-    const container = elem.querySelectorAll(
-      '.bMyTask .list tr.dataRow .dataCell a'
-    )[0];
-    if (container === null) {
-      return;
-    }
-
-    const descFunc = () => {
-      return container.textContent;
-    };
-
-    const projectFunc = () => {
-      return (
-        ($('.accountBlock .mruText') &&
-          $('.accountBlock .mruText').textContent) ||
-        ''
-      );
-    };
-
-    const link = togglbutton.createTimerLink({
-      className: 'salesforce',
-      buttonType: 'minimal',
-      description: descFunc,
-      projectName: projectFunc
-    });
-
-    container.insertBefore(link, container.firstChild);
-  }
-);
-
-// Detail view
-togglbutton.render('#bodyCell:not(.toggl)', { observe: true }, function (elem) {
-  const container = $('.content', elem);
-
-  if (container === null) {
-    return;
-  }
-
-  const parent = $('.pageType', container);
-
-  if (!parent) {
-    return;
-  }
-
-  const descFunc = () => {
-    const desc = $('.pageDescription', container);
-    return desc ? desc.textContent.trim() : '';
-  };
-
-  const projectFunc = () => {
-    return (
-      ($('.accountBlock .mruText') &&
-        $('.accountBlock .mruText').textContent) ||
-      ''
-    );
-  };
-
-  const link = togglbutton.createTimerLink({
-    className: 'salesforce',
-    description: descFunc,
-    projectName: projectFunc
-  });
-
-  parent.appendChild(link);
-});
-
-// Lightning Task Detail view
-togglbutton.render(
-  '.runtime_sales_activitiesTaskCommon:not(.toggl)',
-  { observe: true },
-  function (elem) {
-    const getDescription = () => {
-      return $('.subject .uiOutputText', elem).textContent.trim();
-    };
-
-    const link = togglbutton.createTimerLink({
-      className: 'salesforce-lightning',
-      description: getDescription,
-      buttonType: 'minimal'
-    });
-
-    $('.left', elem).appendChild(link);
-  }
-);
-
 // Lightning Task List view/sidebar
 togglbutton.render(
   '.slds-split-view__list-item:not(.toggl)',
@@ -100,11 +10,44 @@ togglbutton.render(
     };
 
     const link = togglbutton.createTimerLink({
-      className: 'salesforce-lightning-list',
+      className: 'salesforce-list',
       description: getDescription,
       buttonType: 'minimal'
     });
 
     $('.uiOutputText', elem).parentElement.parentElement.appendChild(link);
   }
+);
+
+togglbutton.render(
+  '.slds-page-header__title:not(.toggl)',
+  { observe: true },
+  function (elem) {
+    const description = $('.uiOutputText:not(.selectedListView)', elem);
+    if (!description) {
+      // Looks like a title not relevant to a record, ignore.
+      return;
+    }
+
+    const getDescription = () => {
+      return description.textContent.trim();
+    };
+    const getProject = () => {
+      let project = $('[title="Account Name"]');
+      if (!project) project = $('[title="Related To"]');
+
+      if (project) {
+        return project.nextSibling.textContent.trim();
+      }
+      return getDescription();
+    };
+
+    const link = togglbutton.createTimerLink({
+      className: 'salesforce',
+      description: getDescription,
+      projectName: getProject,
+      buttonType: 'minimal'
+    });
+    description.appendChild(link);
+  }, 'header,.active,.oneRecordHomeFlexipage'
 );

13a9c3c799699549041d91bda2ff1303f42fd034
Shantanu Raj <s@sraj.me>
2019-02-14 05:27:28 +0530

    Partly reverts 0b60bdc
    - Restores salesforce legacy integration

--- src/scripts/content/salesforce.js
+++ src/scripts/content/salesforce.js
@@ -1,5 +1,78 @@
 'use strict';
 
+// Salesforce legacy view
+
+// Updated Listing view
+togglbutton.render(
+  '.bMyTask .list tr.dataRow:not(.toggl)',
+  { observe: true },
+  function (elem) {
+    const container = elem.querySelectorAll(
+      '.bMyTask .list tr.dataRow .dataCell a'
+    )[0];
+    if (container === null) {
+      return;
+    }
+
+    const descFunc = () => {
+      return container.textContent;
+    };
+
+    const projectFunc = () => {
+      return (
+        ($('.accountBlock .mruText') &&
+          $('.accountBlock .mruText').textContent) ||
+        ''
+      );
+    };
+
+    const link = togglbutton.createTimerLink({
+      className: 'salesforce-legacy',
+      buttonType: 'minimal',
+      description: descFunc,
+      projectName: projectFunc
+    });
+
+    container.insertBefore(link, container.firstChild);
+  }
+);
+
+// Detail view
+togglbutton.render('#bodyCell:not(.toggl)', { observe: true }, function (elem) {
+  const container = $('.content', elem);
+
+  if (container === null) {
+    return;
+  }
+
+  const parent = $('.pageType', container);
+
+  if (!parent) {
+    return;
+  }
+
+  const descFunc = () => {
+    const desc = $('.pageDescription', container);
+    return desc ? desc.textContent.trim() : '';
+  };
+
+  const projectFunc = () => {
+    return (
+      ($('.accountBlock .mruText') &&
+        $('.accountBlock .mruText').textContent) ||
+      ''
+    );
+  };
+
+  const link = togglbutton.createTimerLink({
+    className: 'salesforce-legacy',
+    description: descFunc,
+    projectName: projectFunc
+  });
+
+  parent.appendChild(link);
+});
+
 // Lightning Task List view/sidebar
 togglbutton.render(
   '.slds-split-view__list-item:not(.toggl)',

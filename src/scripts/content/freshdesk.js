b2e7b9ff85ecf4a173e7d2b9d12d9ad96f1ca68e
Tyler Crammond <tylerchc92@gmail.com>
2019-02-15 13:58:31 +0000

    Fix Freshdesk Mint 2019-02 UI
    Closes #1099 (again).

--- src/scripts/content/freshdesk.js
+++ src/scripts/content/freshdesk.js
@@ -1,16 +1,15 @@
 'use strict';
+/* global createTag */
 
-togglbutton.render('#Pagearea:not(.toggl)', { observe: true }, function(elem) {
-  var link,
-    container = createTag('li', 'ticket-btns'),
-    description,
-    titleElem = $('h2.subject', elem),
-    idElem = $('#ticket-display-id'),
-    projectElem = $('.logo_text'),
-    buttonsElem = $('.ticket-actions > ul');
+togglbutton.render('#Pagearea:not(.toggl)', { observe: true }, function (elem) {
+  const container = createTag('li', 'ticket-btns');
+  const titleElem = $('h2.subject', elem);
+  const idElem = $('#ticket-display-id');
+  const projectElem = $('.logo_text');
+  const buttonsElem = $('.ticket-actions > ul');
+  const description = idElem.textContent.trim() + ' ' + titleElem.textContent.trim();
 
-  description = idElem.textContent.trim() + ' ' + titleElem.textContent.trim();
-  link = togglbutton.createTimerLink({
+  const link = togglbutton.createTimerLink({
     className: 'freshdesk',
     description: description,
     projectName: projectElem && projectElem.textContent.trim(),
@@ -23,31 +22,37 @@ togglbutton.render('#Pagearea:not(.toggl)', { observe: true }, function(elem) {
 
 // Freshdesk mint (late 2018)
 togglbutton.render('.page-actions__left:not(.toggl)', { observe: true }, elem => {
-  const descriptionElem = $('.description-subject');
+  const descriptionElem = $('.ticket-subject-heading');
 
   // if there's no description element it's overview page, don't show
-  if (!descriptionElem) { return }
+  if (!descriptionElem) {
+    return;
+  }
 
   const descriptionSelector = () => {
     const ticketNumber = $('.breadcrumb__item.active').textContent.trim();
-    const subject = $('.description-subject').textContent.trim();
+    const subject = $('.ticket-subject-heading').textContent.trim();
     return `${ticketNumber} ${subject}`;
-  }
+  };
 
   const link = togglbutton.createTimerLink({
     className: 'freshdesk__mint',
     description: descriptionSelector,
     buttonType: 'minimal',
     tags: () => {
-      const tagList = $('.list-items');
-
+      const tagList = $('.ticket-tags ul');
       if (!tagList ||
         !tagList.children ||
         !tagList.children.length) { return []; }
 
-      return [...tagList.children].map(child => child.textContent);
+      return [...tagList.querySelectorAll('li')]
+        .map(listItem => {
+          const content = listItem.querySelector('.tag-options');
+          const tag = content ? content.textContent : '';
+          return tag.trim().replace(/[\r\n\t]/ig, ''); /* UI has strange characters in the markup, let's avoid it */
+        });
     }
-  })
+  });
 
   elem.appendChild(link);
 });

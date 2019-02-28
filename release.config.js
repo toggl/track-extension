9acd3796391abd4f10acee9e5309af82db2e4f2f
Shantanu Raj <s@sraj.me>
2019-02-28 17:40:29 +0530

    chore(docs): Move docs to master
    - chore(docs): keep modified changelog from ci
    - fix(github): Fix firefox zip path

--- release.config.js
+++ release.config.js
@@ -2,6 +2,12 @@ module.exports = {
   'plugins': [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        'changelogFile': 'docs/_includes/changelog.md'
+      }
+    ],
     '@semantic-release/npm',
     [
       '@semantic-release/github',
@@ -12,13 +18,21 @@ module.exports = {
             'label': 'Toggl Button for Chrome'
           },
           {
-            'path': 'dist/toggl-button-chrome-*.zip',
+            'path': 'dist/toggl-button-firefox-*.zip',
             'label': 'Toggl Button for Firefox'
           }
         ]
       }
     ],
-    '@semantic-release/git'
+    [
+      '@semantic-release/git',
+      {
+        'assets': [
+          'docs/_includes/changelog.md',
+          'package.json'
+        ]
+      }
+    ]
   ],
   'tagFormat': '${version}' // eslint-disable-line no-template-curly-in-string
 };

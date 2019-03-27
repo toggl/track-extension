1110f79c2b9e113c4832055c5a15e4dcf5fb4616
Henrique Pinheiro <henrique@gampoc.com>
2019-03-27 18:54:59 -0300

    fix(wordpress): Fix wordpress integration
    Button wasn't showing up on WP
    Fixed for WP 4.9.x and 5.1.x
    
    Closes #1362

--- src/scripts/content/wordpress.js
+++ src/scripts/content/wordpress.js
@@ -1,22 +1,34 @@
 'use strict';
 
-togglbutton.render('.editor__header:not(.toggl)', { observe: true }, function (
-  elem
-) {
-  const tabs = $('.editor__switch-mode', elem);
-
+// WP 4.9
+togglbutton.render('#poststuff:not(.toggl)', { observe: true }, function (elem) {
+  const heading = document.querySelector('.wp-heading-inline');
   const description = function () {
-    return document.querySelector('.editor-title__input').value;
+    return elem.querySelector('#title').value;
   };
-
-  if ($('.toggl-button')) {
-    return;
-  }
 
   const link = togglbutton.createTimerLink({
     className: 'wordpress',
     description: description
   });
 
-  tabs.parentElement.insertBefore(link, tabs);
+  heading.append(link);
+});
+
+// WP 5.1
+togglbutton.render('.edit-post-header:not(.toggl)', { observe: true }, function (
+  elem
+) {
+  const targetElement = elem.querySelector('.edit-post-header__settings');
+  const description = function () {
+    const titleInput = document.getElementById('post-title-0');
+    return titleInput ? titleInput.value : '';
+  };
+
+  const link = togglbutton.createTimerLink({
+    className: 'wordpress',
+    description: description
+  });
+
+  targetElement.prepend(link);
 });

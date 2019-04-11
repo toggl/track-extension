29c579de8ad388696980137fe620023401e20d04
Samuel Szabo <samuelszabo7+git@gmail.com>
2019-04-11 11:26:42 +0200

    fix(wrike): Get title from ws-task-title

--- src/scripts/content/wrike.js
+++ src/scripts/content/wrike.js
@@ -6,6 +6,10 @@ togglbutton.render('.wspace-task-view:not(.toggl)', { observe: true }, function 
   const container = $('.wrike-panel-header-toolbar', elem);
 
   const titleElem = function () {
+    const wsTaskTitle = document.querySelectorAll('ws-task-title');
+    if (wsTaskTitle.length === 1 && wsTaskTitle[0].textContent !== '') {
+      return wsTaskTitle[0].textContent;
+    }
     return $('title').textContent.replace(' - Wrike', '');
   };
 

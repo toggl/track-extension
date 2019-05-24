a5b52ddd2715089190db7379588a20697899883a
Nur Rony <pro.nmrony@gmail.com>
2019-05-24 15:16:38 +0600

    fix(fibery): change selector to grab description in more generic way

--- src/scripts/content/fibery.js
+++ src/scripts/content/fibery.js
@@ -5,16 +5,13 @@
 const renderTogglButton = () => {
   togglbutton.render('div.ObjectEditor:not(.toggl)', { observe: true }, $container => {
     const descriptionSelector = () => {
-      const $description =
-        $('.HeaderLayout textarea[name="product-development/name"]', $container) ||
-        $('.HeaderLayout textarea[name="Wiki/name"]', $container);
-      return $description.textContent.trim();
+      const $description = $('.HeaderLayout textarea', $container);
+      return $description ? $description.textContent.trim() : '';
     };
 
     const link = togglbutton.createTimerLink({
       className: 'fibery',
       description: descriptionSelector
-      // projectName: projectSelector
     });
 
     $('div.ObjectEditorHeader > .HeaderLayout').appendChild(link);

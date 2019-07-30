0d2c5f6b62d2aab73eaaff9d206350f6165b0a79
Wieland Lindenthal <w.lindenthal@forkmerge.com>
2019-07-31 00:37:22 +0200

    fix(OpenProject): Remove toggl button from work package list.
    - Add work package ID to time entry description to easier find work packages.

--- src/scripts/content/openproject.js
+++ src/scripts/content/openproject.js
@@ -1,35 +1,13 @@
 'use strict';
 
-// Work packages list items
-togglbutton.render(
-  'table.work-package-table tbody tr td.id:not(.toggl)',
-  { observe: true },
-  function (elem) {
-    const container = elem;
-    const description = $(
-      'span[data-field-name="subject"]',
-      elem.parentNode
-    ).textContent.trim();
-    const projectName = $('#projects-menu').title.trim();
-
-    const link = togglbutton.createTimerLink({
-      className: 'openproject',
-      description: description,
-      projectName: projectName,
-      buttonType: 'minimal'
-    });
-
-    container.appendChild(link);
-  }
-);
-
 // Work packages details view
 togglbutton.render(
   '.work-packages--show-view:not(.toggl)',
   { observe: true },
   function (elem) {
+    const workPackageId = $('.work-packages--info-row > span:first-of-type').textContent.trim();
     const container = $('.attributes-group--header', elem);
-    const description = $('.subject').textContent.trim();
+    const description = '[OP' + workPackageId + '] ' + $('.subject').textContent.trim();
     const projectName = $('#projects-menu').title.trim();
 
     const link = togglbutton.createTimerLink({

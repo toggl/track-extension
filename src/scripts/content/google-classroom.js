08f09493c9392013b540bb4d4d60ef91f6ceda50
RetroCraft <retrocraft314@gmail.com>
2019-09-07 16:57:39 -0400

    feat(google-classroom): new integration

---
+++ src/scripts/content/google-classroom.js
@@ -0,0 +1,19 @@
+'use strict';
+
+togglbutton.render('div[data-without-stream-item-materials]', { observe: true }, elem => {
+  if ($('.toggl-button', elem)) {
+    return;
+  }
+
+  const titleFunc = () => {
+    const title = $('h1', elem);
+    return title ? title.textContent.trim() : '';
+  };
+
+  const link = togglbutton.createTimerLink({
+    className: 'google-classroom',
+    description: titleFunc
+  });
+  console.log('trigger', link);
+  $('.rec5Nb.QRiHXd', elem).appendChild(link);
+});

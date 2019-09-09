452e45d9cda1caf69df6960c87cafdf5ec8a444d
Marián Skrip <marian.skripp@gmail.com>
2019-09-09 13:16:15 +0200

    feat: Enable gmail integration to work with Drag-app popup view

--- src/scripts/content/google-mail.js
+++ src/scripts/content/google-mail.js
@@ -16,3 +16,19 @@ togglbutton.render('.ha:not(.toggl)', { observe: true }, function (elem) {
 
   elem.appendChild(link);
 });
+
+// This enables support for popup gmail view used by dragapp.com extension
+togglbutton.render('div.subject:not(.toggl)', { observe: true }, function (elem) {
+  const description = $('span', elem);
+
+  if (!description) {
+    return;
+  }
+
+  const link = togglbutton.createTimerLink({
+    className: 'google-mail',
+    description: description.textContent
+  });
+
+  elem.children[0].insertBefore(link, elem.children[0].children[1]);
+});

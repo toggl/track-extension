15fe50c05b166c6f05e3889e9beaf14483a1a6df
thiago <371426+dooart@users.noreply.github.com>
2019-09-17 17:46:42 -0300

    Fix observed element for Dynamics 365
    The button was losing the active status right after the timer was started.

--- src/scripts/content/dynamics-365.js
+++ src/scripts/content/dynamics-365.js
@@ -1,17 +1,23 @@
 'use strict';
 
-togglbutton.render('#headerContainer:not(.toggl)',
+togglbutton.render(
+  '[data-id="ticketnumber.fieldControl-text-box-text"]:not(.toggl)',
   { observe: true },
   function (elem) {
+    const header = $('#headerContainer');
     const getDescription = function () {
       // entity: incident
-      const ticketnumber = $('input[data-id="ticketnumber.fieldControl-text-box-text"]');
+      const ticketnumber = $(
+        'input[data-id="ticketnumber.fieldControl-text-box-text"]'
+      );
       const ticketname = $('input[data-id="title.fieldControl-text-box-text"]');
-      if ((ticketnumber) || (ticketname)) {
-        return (ticketnumber ? ticketnumber.title + ' ' : '') + (ticketname ? ticketname.title : '');
+      if (ticketnumber || ticketname) {
+        return (
+          (ticketnumber ? ticketnumber.title + ' ' : '') +
+          (ticketname ? ticketname.title : '')
+        );
       } else {
         // other entities
-        const header = $('#headerContainer');
         if (!header) {
           return '';
         }
@@ -19,12 +25,13 @@ togglbutton.render('#headerContainer:not(.toggl)',
         if (!title) {
           return '';
         }
-        return (title ? title.textContent : '');
+        return title ? title.textContent : '';
       }
     };
     const link = togglbutton.createTimerLink({
       className: 'dynamics365',
       description: getDescription
     });
-    elem.appendChild(link);
-  });
+    header.appendChild(link);
+  }
+);

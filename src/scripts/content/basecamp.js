53d44fdfc2cb7c75eec50fc98b26944d00bfd2e6
thiago <371426+dooart@users.noreply.github.com>
2019-10-03 17:01:00 -0300

    fix(basecamp3): Improve layout and UX
    List of improvements:
    - Prevents overlap with default Basecamp hover actions
    - Uses minimal button style for list
    - Makes button always visible in the card view
    - Makes button visible after the task is completed (so the user doesn't forget to stop the timer) in both list/card views
    
    Closes #1426

--- src/scripts/content/basecamp.js
+++ src/scripts/content/basecamp.js
@@ -67,21 +67,23 @@ togglbutton.render(
 );
 
 // Basecamp 3
-togglbutton.render(
-  '.todos li.todo:not(.toggl):not(.completed)',
-  { observe: true },
-  function (elem) {
-    const parent = $('.checkbox__content', elem);
-    const description = parent.childNodes[1].textContent.trim();
-    let project = $('#a-breadcrumb-menu-button');
-    project = project ? project.textContent : '';
+togglbutton.render('.todos li.todo:not(.toggl)', { observe: true }, function (
+  elem
+) {
+  const parent = $('.checkbox__content', elem);
+  const description = parent.childNodes[1].textContent.trim();
+  let project = $('#a-breadcrumb-menu-button');
+  project = project ? project.textContent : '';
 
-    const link = togglbutton.createTimerLink({
-      className: 'basecamp3',
-      description: description,
-      projectName: project
-    });
+  const metadata = $('.metadata', parent);
 
-    parent.appendChild(link);
-  }
-);
+  const link = togglbutton.createTimerLink({
+    buttonType: metadata ? null : 'minimal',
+    className: 'basecamp3',
+    description: description,
+    projectName: project
+  });
+
+  const container = metadata || parent;
+  container.appendChild(link);
+});

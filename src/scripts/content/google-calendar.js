be4d5d0e0da2e8ac951d4ed010c0115c824885fa
Aaron Yong <a.yong91@gmail.com>
2019-10-29 11:28:25 -0600

    fix(google-calendar): Support Google Calendar detail view
    - Refactored elem selector to support popup and detail view
    - Added implementation notes
    
    Closes #955

--- src/scripts/content/google-calendar.js
+++ src/scripts/content/google-calendar.js
@@ -1,30 +1,75 @@
 'use strict';
 /* global $, togglbutton */
 
-// Popup view Google Calendar Modern
-togglbutton.render('div[data-chips-dialog="true"]', { observe: true }, elem => {
-  // We manually check for presence of button, as one popup element
-  // gets recycled by calendar when clicking between different calendar entries.
-  if ($('.toggl-button', elem)) {
-    return;
+/**
+ * Google Calendar Modern
+ *
+ * Detail view and popup view support.
+ *
+ * Implementation notes:
+ * - The popup view is re-used when a user clicks between each event without
+ *   dismissing the popup.
+ * - The way Calendar re-uses the popup view causes the .toggl class to
+     remain on the popup div.
+ * - Detail view's container does not appear to be re-used, so the pseudo-class
+ *   selector (i.e. ":not(.toggl)") works for detail view.
+ * - When implementing a selector, do not select on an aria-label's value;
+ *   i18n will cause that selector to fail.
+ */
+
+const notTogglPseudoClass = ':not(.toggl)';
+const popupDialogSelector = 'div[data-chips-dialog="true"]';
+const detailContainerSelector = 'div[data-is-create="false"]';
+const rootLevelSelectors = [
+  `${popupDialogSelector}`,
+  `${detailContainerSelector}${notTogglPseudoClass}`
+].join(',');
+
+togglbutton.render(rootLevelSelectors, { observe: true }, elem => {
+  const elemIsPopup = $(popupDialogSelector, elem.parentElement);
+  const elemIsDetail = $(detailContainerSelector, elem.parentElement);
+  let getDescription;
+  let target;
+
+  if (elemIsPopup) {
+    // Popup selector reaches here repeatedly, so we need to prevent the
+    // creation of more than one Toggl button for a popup view.
+    if ($('.toggl-button', elem)) {
+      return;
+    }
+
+    const closeButton = $('[aria-label]:last-child', elem);
+
+    getDescription = () => {
+      const titleSpan = $('span[role="heading"]', elem);
+      return titleSpan ? titleSpan.textContent.trim() : '';
+    };
+    target = closeButton.parentElement.nextSibling; // Left of the left-most action
+  } else if (elemIsDetail) {
+    const closeButton = $('div[aria-label]', elem);
+
+    getDescription = () => {
+      const titleInput = $('input[data-initial-value]', elem);
+      return titleInput ? titleInput.value.trim() : '';
+    };
+    target =
+      closeButton.parentElement.nextElementSibling.lastElementChild
+        .lastElementChild; // Date(s)/All day section
   }
 
-  // Grab the "toolbar" by finding "Close" button, which is _always_ present.
-  // Note: do not use the aria-label values, it will break with i18n.
-  const target = $('[aria-label]:last-child', elem).parentElement.nextSibling;
   if (!target || target.tagName.toLowerCase() !== 'div') {
-    // Looks like the "create event" dialog, don't attempt to insert a button.
+    // Prevent adding a Toggl button to other dialogs/views, e.g. Create Event
     return;
   }
-
-  const getDescription = () => {
-    const title = $('span[role="heading"]', elem);
-    return title ? title.textContent.trim() : '';
-  };
 
   const link = togglbutton.createTimerLink({
     className: 'google-calendar-modern',
     description: getDescription
   });
-  target.prepend(link);
+
+  if (elemIsPopup) {
+    target.prepend(link);
+  } else if (elemIsDetail) {
+    target.appendChild(link);
+  }
 });

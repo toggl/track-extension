b0a3bedd59be25913e37f8b599f75d980e65b4cd
lise-toggl <lisamei@toggl.com>
2019-11-05 10:43:24 +1100

    feat(todoist): Add button to task view

--- src/scripts/content/todoist.js
+++ src/scripts/content/todoist.js
@@ -3,6 +3,7 @@
 
 const todoistEditor = document.getElementById('content');
 
+// main list view
 togglbutton.render(
   '.task_item .task_item_actions:not(.toggl)',
   { observe: true, observeTarget: todoistEditor, debounceInterval: 100 },
@@ -15,9 +16,7 @@ togglbutton.render(
     const tagsSelector = () => {
       const tags = content.querySelectorAll('a.label');
 
-      return [...tags].map(tag => {
-        return tag.textContent;
-      });
+      return [...tags].map(tag => tag.textContent);
     };
 
     const link = togglbutton.createTimerLink({
@@ -29,10 +28,86 @@ togglbutton.render(
     });
 
     const wrapper = document.createElement('div');
-    wrapper.classList.add('task_info', 'task_content_item');
+    wrapper.classList.add('task_content_item');
     wrapper.style.display = 'flex';
     wrapper.style.alignItems = 'center';
     wrapper.style.justifyContent = 'center';
+    wrapper.appendChild(link);
+
+    elem.insertBefore(wrapper, elem.firstChild);
+  }
+);
+
+// task view
+togglbutton.render('.item_detail:not(.toggl)', { observe: true }, elem => {
+  const container = elem.querySelector('.item_actions');
+
+  const descriptionSelector = () =>
+    elem.querySelector('.item_overview_content').firstChild.textContent;
+
+  const tagsSelector = () => {
+    const tags = elem.querySelectorAll('a.item_overview_label');
+
+    return [...tags].map(tag => tag.textContent);
+  };
+
+  const project = getParentIfProject(elem);
+
+  const link = togglbutton.createTimerLink({
+    className: 'todoist-detail',
+    description: descriptionSelector,
+    projectName: project,
+    tags: tagsSelector,
+    buttonType: 'minimal'
+  });
+
+  const wrapper = document.createElement('div');
+  wrapper.classList.add('item_action');
+  wrapper.style.display = 'flex';
+  wrapper.style.alignItems = 'center';
+  wrapper.style.justifyContent = 'center';
+  wrapper.appendChild(link);
+
+  if (container) {
+    container.insertBefore(wrapper, container.firstChild);
+  }
+});
+
+// task view - subtasks
+togglbutton.render(
+  '.task_list_item .task_list_item__actions:not(.toggl)',
+  { observe: true, observeTarget: todoistEditor, debounceInterval: 100 },
+  elem => {
+    const content = elem.closest('.task_list_item').querySelector('.task_list_item__content');
+
+    const descriptionSelector = () => content.querySelector('.task_list_item__text').textContent;
+
+    let project = '';
+    const projectId = elem.closest('.task_list_item').getAttribute('data-item-id');
+    if (document.getElementById(`item_${projectId}`)) {
+      const projectContent = document.getElementById(`item_${projectId}`).querySelector('.content');
+      project = getProjectNames(projectContent);
+    } else {
+      project = getParentIfProject(elem.closest('.item_detail'));
+    }
+
+    const tagsSelector = () => {
+      const tags = content.querySelectorAll('.task_list_item__info_tags__label');
+
+      return [...tags].map(tag => tag.textContent);
+    };
+
+    const link = togglbutton.createTimerLink({
+      className: 'todoist-detail-subtask',
+      description: descriptionSelector,
+      projectName: project,
+      buttonType: 'minimal',
+      tags: tagsSelector
+    });
+
+    const wrapper = document.createElement('div');
+    wrapper.style.display = 'flex';
+    wrapper.style.marginTop = '3px';
     wrapper.appendChild(link);
 
     elem.insertBefore(wrapper, elem.firstChild);
@@ -143,4 +218,15 @@ function getProjectNames (elem) {
     }
     return projectNames;
   };
+}
+
+function getParentIfProject (elem) {
+  const parent = elem.querySelector('.item_detail_parent_info');
+  let project = '';
+
+  if (parent.querySelector('circle, .item_detail_parent_icon__inbox_icon')) {
+    project = parent.querySelector('.item_detail_parent_name').textContent;
+  }
+
+  return project;
 }

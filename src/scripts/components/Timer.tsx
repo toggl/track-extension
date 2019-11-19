fbfa047bbd42afc64301df4d015fa938b30f5f14
Shantanu Raj <s@sraj.me>
2019-11-20 01:10:58 +0530

    fix(code): TS error

--- src/scripts/components/Timer.tsx
+++ src/scripts/components/Timer.tsx
@@ -44,10 +44,10 @@ function RunningTimer(props: { entry: Toggl.TimeEntry, project: Toggl.Project | 
   const { entry, project } = props;
   const tags = (entry.tags || []).join(', ');
 
-  const editEntry = (e: React.MouseEvent) => {
+  const editEntry = () => {
     window.PopUp.renderEditForm(entry);
   };
-  const stopTimer = (e) => {
+  const stopTimer = (e: React.MouseEvent) => {
     e.preventDefault();
     e.stopPropagation();
     window.PopUp.sendMessage({ type: 'stop', service: 'dropdown', respond: true });

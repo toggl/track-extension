f25be55e58d020cab982a71594e92e16a2e7d9e9
Marián Skrip <marian.skripp@gmail.com>
2019-11-28 13:34:54 +0100

    feat: Add option to integrate with private projects with generic integration
    - Create `Generic Toggl` integration
    
    Close #1524

---
+++ src/scripts/content/generic-toggl.js
@@ -0,0 +1,24 @@
+'use strict';
+
+togglbutton.render('.toggl-root:not(.toggl)', { observe: true }, function (
+  elem
+) {
+  console.log(elem);
+  const integrationClassName = elem.getAttribute('data-class-name');
+  const description = elem.getAttribute('data-description');
+  const projectName = elem.getAttribute('data-project-name');
+  const projectId = elem.getAttribute('data-project-id');
+  const tags = elem.getAttribute('data-tags');
+  const type = elem.getAttribute('data-type');
+
+  const link = togglbutton.createTimerLink({
+    className: integrationClassName || 'generic-toggl',
+    description: description,
+    projectName: projectName,
+    projectId: parseInt(projectId, 10),
+    tags: tags !== null ? tags.split(',') : [],
+    buttonType: type
+  });
+
+  elem.appendChild(link);
+});

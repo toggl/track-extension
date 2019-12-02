cfafa186a51608d76ac46c3dc883f81e307b66fe
Maciej Rylko <rylko.maciej@gmail.com>
2019-12-02 11:00:33 +0100

    fix(worksection): Fix worksection integration after layout change
    Closes #1496

--- src/scripts/content/worksection.js
+++ src/scripts/content/worksection.js
@@ -1,21 +1,59 @@
 'use strict';
 
-togglbutton.render('.task:not(.toggl)', { observe: true }, function (elem) {
-  const projectElem = $('#client_name b');
-  let taskElem = $('.task h1');
+// Gets project name from print styles
+// They're included on any page as of 2019-12-02 with following structure:
+// <div id="header_print">
+//   <span>{workspace_name}</span>
+//   projectName
+// </div>
+function getProjectName () {
+  const printHeaderElem = document.querySelector('#header_print');
 
-  if (!taskElem) {
-    return;
+  return printHeaderElem && printHeaderElem.lastChild && printHeaderElem.lastChild.nodeValue
+    ? printHeaderElem.lastChild.nodeValue.trim()
+    : '';
+}
+
+// Task view
+togglbutton.render('.task:not(.toggl)', { observe: true }, function (elem) {
+  function getDescription () {
+    // Task details view
+    const descriptionElem = elem.querySelector('h1 > a');
+    if (descriptionElem) return descriptionElem.textContent.trim();
+
+    // Task modal view
+    const descriptionModalElem = document.querySelector('#TB_title .name a');
+    return descriptionModalElem ? descriptionModalElem.textContent.trim() : '';
   }
 
-  taskElem = taskElem.childNodes[2].textContent.trim();
+  const link = togglbutton.createTimerLink({
+    className: 'worksection-taskview',
+    description: getDescription,
+    projectName: getProjectName
+  });
+
+  const root = document.querySelector('.buts_menu');
+  if (root) {
+    root.appendChild(link);
+  }
+});
+
+// Tasks list
+togglbutton.render('#tasks_images .item:not(.toggl)', { observe: true }, function (elem) {
+  function getDescription () {
+    const descriptionElem = elem.querySelector('.name > a');
+    return descriptionElem ? descriptionElem.textContent.trim() : '';
+  }
 
   const link = togglbutton.createTimerLink({
-    className: 'worksection',
-    description: taskElem,
-    projectName: projectElem && projectElem.textContent.trim()
+    buttonType: 'minimal',
+    className: 'worksection-listview',
+    description: getDescription,
+    projectName: getProjectName
   });
-  link.classList.add('norm');
 
-  $('#tmenu2', elem).appendChild(link);
+  const root = elem.querySelector('.td_actions > span');
+  if (root) {
+    root.appendChild(link);
+  }
 });

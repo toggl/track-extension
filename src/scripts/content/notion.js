b6a632964cc353f66abc4682e4df2ff6cbc6be11
lise-toggl <lisamei@toggl.com>
2020-01-07 16:57:43 +1100

    fix(notion): Return header button to popup pages and add styles

--- src/scripts/content/notion.js
+++ src/scripts/content/notion.js
@@ -1,6 +1,33 @@
 'use strict';
 
-// Button renders left of page title
+// Selectors here are madness, it works for as of Dec 4th 2019
+// Button renders in popup/dialog view
+togglbutton.render(
+  '.notion-peek-renderer:not(.toggl)',
+  { observe: true },
+  function (elem) {
+    function getDescription () {
+      const descriptionElem = elem.querySelector('.notion-scroller .notion-selectable div[contenteditable="true"]');
+      return descriptionElem ? descriptionElem.textContent.trim() : '';
+    }
+
+    const link = togglbutton.createTimerLink({
+      className: 'notion',
+      description: getDescription
+    });
+
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('toggl-button-notion-wrapper');
+    wrapper.appendChild(link);
+
+    const root = elem.querySelector('div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3)');
+    if (root) {
+      root.prepend(wrapper);
+    }
+  }
+);
+
+// Button renders left of page title - hidden on popups with css
 togglbutton.render(
   '.notion-page-controls + div:not(.toggl)',
   { observe: true },

1bb62dea011c8ec3964b33c350a0ef7dd2fbdef2
Kanato Masayoshi <42759138+cosnomi@users.noreply.github.com>
2020-03-18 10:38:02 +0900

    Update kanbanist integration (#1685)
    * Update kanbanist integration
    
    * Add style for .active button

--- src/scripts/content/kanbanist.js
+++ src/scripts/content/kanbanist.js
@@ -1,6 +1,9 @@
 'use strict';
 /* global togglbutton, $ */
 
+const stylesheet = document.styleSheets.item(document.styleSheets.length - 1);
+stylesheet.insertRule('.toggl-button.active {visibility:visible !important}');
+
 togglbutton.render(
   '.ListItem:not(.toggl)',
   { observe: true },
@@ -15,15 +18,23 @@ togglbutton.render(
       return $project.textContent.trim();
     };
 
-    const linkStyle = 'margin-left: 5px';
+    const linkStyle = 'margin-left: 5px;visibility: hidden';
     const link = togglbutton.createTimerLink({
       className: 'kanbanist',
       description: descriptionSelector,
       projectName: projectSelector,
       buttonType: 'minimal'
     });
-    link.classList.add('task-link', 'sr-only', 'sr-only-focusable');
+    link.classList.add('task-link');
     link.setAttribute('style', linkStyle);
-    $('.task-link', $container).after(link);
+    $('.ListItem-text', $container).after(link);
+    $container.onmouseenter = () => {
+      console.log(link.style.visibility);
+      link.style.visibility = 'visible';
+    };
+    $container.onmouseleave = () => {
+      console.log(link.style.visibility);
+      link.style.visibility = 'hidden';
+    };
   }
 );

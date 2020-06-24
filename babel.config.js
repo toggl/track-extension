48a8cd90709cc5c17b2c934d2ae7ada567edf58a
Tyler Crammond <tylerchc92@gmail.com>
2020-06-24 11:26:50 +0100

    chore(deps): Install and configure Jest

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+// For Jest.
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current'
+        }
+      }
+    ],
+    '@babel/preset-typescript'
+  ]
+};

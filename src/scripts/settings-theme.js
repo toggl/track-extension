ca90fedd9ff4bcfcef37b89bc74b7357cfbf5aca
Shantanu Raj <s@sraj.me>
2020-06-30 12:05:53 +0530

    chore(theme): Default to system theme if no preference is set

--- src/scripts/settings-theme.js
+++ src/scripts/settings-theme.js
@@ -1,2 +1,3 @@
-const darkMode = JSON.parse(localStorage.getItem('darkMode'));
+let darkMode = JSON.parse(localStorage.getItem('darkMode'));
+if (darkMode === null) darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
 darkMode && document.documentElement.classList.add('dark');

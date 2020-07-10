03ad69527588664f82de12605d2c964557005f5e
Shantanu Raj <s@sraj.me>
2020-07-10 12:07:01 +0530

    fix(theme): Type error on updating doc.body

--- src/scripts/settings-theme.js
+++ src/scripts/settings-theme.js
@@ -2,5 +2,5 @@ let darkMode = JSON.parse(localStorage.getItem('darkMode'));
 if (darkMode === null) darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
 if (darkMode) {
   document.documentElement.classList.add('dark');
-  document.body.classList.add('dark');
+  document.body && document.body.classList.add('dark');
 }

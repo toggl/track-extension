8e4dded98bff581b953179d76a8f83a6450fe267
thiago <371426+dooart@users.noreply.github.com>
2020-08-15 00:56:12 -0300

    chore(refactor): Make Toggl Plan integration declarative

--- src/scripts/content/toggl-plan.js
+++ src/scripts/content/toggl-plan.js
@@ -2,31 +2,17 @@
 
 // Changed from teamweek to Toggl-Plan
 // Version active on July 2020
-togglbutton.render('.task-form:not(.toggl)',
-  { observe: true },
-  element => {
-    const container = element.querySelector('[data-hook=actions-menu]');
-
-    const getDescriptionText = () => {
-      const titleElement = element.querySelector('[data-hook=name-editor] textarea');
-      return titleElement ? titleElement.textContent.trim() : '';
-    };
-
-    const getProjectText = () => {
-      const planElement = element.querySelector('[data-hook=plan-editor] [data-hook=input-value]');
-
-      return planElement
-        ? planElement.textContent.trim()
-        : '';
-    };
-
-    const link = togglbutton.createTimerLink({
-      className: 'toggl-plan',
-      buttonType: 'minimal',
-      description: getDescriptionText,
-      projectName: getProjectText
-    });
-
-    container.parentNode.insertBefore(link, container);
+togglbutton.declare({
+  elements: {
+    title: '[data-hook=name-editor] textarea',
+    plan: '[data-hook=plan-editor] [data-hook=input-value]'
+  },
+  link: {
+    observe: '.task-form:not(.toggl)',
+    target: '[data-hook=actions-menu]',
+    targetInsertion: 'before',
+    className: 'toggl-plan',
+    description: '{{title}}',
+    projectName: '{{plan}}'
   }
-);
+});

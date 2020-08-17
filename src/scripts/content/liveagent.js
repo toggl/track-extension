fd9554fe6931dfdab2822af03765957d64af02a3
naberal <hmelarvova@gmail.com>
2020-08-17 11:27:55 +0300

    perf: LiveAgent integration
    remove duplicate
    remove '$' to 'document.querySelector'
    add try/catch

--- src/scripts/content/liveagent.js
+++ src/scripts/content/liveagent.js
@@ -1,41 +1,6 @@
 'use strict';
 
-// ticket
-togglbutton.render(
-  '.ConversationDetailsView:not(.toggl)',
-  { observe: true },
-  function (
-    elem
-  ) {
-    const descriptionSelector = () => {
-      const ticketCode = $('.CodeLabel').textContent.trim();
-      const ticketSubject = $('.ConversationHeaderSubject').textContent.trim();
-      return '[' + ticketCode + '] ' + ticketSubject;
-    };
-
-    const link = togglbutton.createTimerLink({
-      className: 'liveagent',
-      description: descriptionSelector
-    });
-
-    const wrapper = document.createElement('div');
-    wrapper.className = 'togglWrapper';
-    wrapper.style.textAlign = 'center';
-    wrapper.style.borderBottom = '1px solid rgba(125,125,125,0.3)';
-    wrapper.style.marginBottom = '1em';
-    wrapper.appendChild(link);
-
-    elem.insertBefore(wrapper, elem.firstChild);
-  }
-);
-
-// knowledgebase article dialog
-togglbutton.render('.ArticleDetails:not(.toggl)', { observe: true }, (articleDetails) => {
-  const link = togglbutton.createTimerLink({
-    className: 'liveagent',
-    description: $('.ArticleContentView .DialogTitle').textContent.trim()
-  });
-
+const button = (link, elem) => {
   const wrapper = document.createElement('div');
   wrapper.className = 'togglWrapper';
   wrapper.style.textAlign = 'center';
@@ -43,5 +8,44 @@ togglbutton.render('.ArticleDetails:not(.toggl)', { observe: true }, (articleDet
   wrapper.style.marginBottom = '1em';
   wrapper.appendChild(link);
 
-  articleDetails.insertBefore(wrapper, articleDetails.firstChild);
+  elem.insertBefore(wrapper, elem.firstChild);
+};
+
+// ticket
+const descriptionSelector = () => {
+  try {
+    const ticketCode = document.querySelector('.CodeLabel').textContent.trim();
+    const ticketSubject = document.querySelector('.ConversationHeaderSubject').textContent.trim();
+    return '[' + ticketCode + '] ' + ticketSubject;
+  } catch (e) {
+    return '';
+  }
+};
+
+togglbutton.render(
+  '.ConversationDetailsView:not(.toggl)', { observe: true }, (elem) => {
+    const link = togglbutton.createTimerLink({
+      className: 'liveagent',
+      description: descriptionSelector
+    });
+
+    button(link, elem);
+  }
+);
+
+// knowledgebase article dialog
+const knowledgeBaseName = () => {
+  try {
+    return document.querySelector('.ArticleContentView .DialogTitle').textContent.trim();
+  } catch (e) {
+    return '';
+  }
+};
+togglbutton.render('.ArticleDetails:not(.toggl)', { observe: true }, (articleDetails) => {
+  const link = togglbutton.createTimerLink({
+    className: 'liveagent',
+    description: knowledgeBaseName
+  });
+
+  button(link, articleDetails);
 });

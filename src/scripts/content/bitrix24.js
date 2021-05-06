fab0755fdf80831d27df6ed2a60dad61852261b6
Henrique Pinheiro <henrique@gampoc.com>
2021-05-06 17:50:05 +0100

    fix(bitrix): Fix Bitrix iframes
    Closes #1883

--- src/scripts/content/bitrix24.js
+++ src/scripts/content/bitrix24.js
@@ -1,19 +1,30 @@
 'use strict';
 /* global togglbutton, $ */
 
+const addButton = ($container, iframeSelector) => {
+  const link = togglbutton.createTimerLink({
+    className: 'bitrix24',
+    description: descriptionSelector,
+    projectName: projectSelector,
+    tags: tagsSelector
+  }, iframeSelector);
+
+  $container.appendChild(link);
+};
+
 togglbutton.render(
   '.task-detail .task-view-buttonset:not(.toggl)',
   { observe: true },
-  $container => {
-    const link = togglbutton.createTimerLink({
-      className: 'bitrix24',
-      description: descriptionSelector,
-      projectName: projectSelector,
-      tags: tagsSelector
-    });
+  addButton
+);
 
-    $container.appendChild(link);
-  }
+togglbutton.render(
+  '.task-detail .task-view-buttonset:not(.toggl)',
+  {
+    observe: true,
+    iframeSelector: 'iframe.side-panel-iframe:not(.toggl)'
+  },
+  $container => addButton($container, 'iframe.side-panel-iframe')
 );
 
 function descriptionSelector () {

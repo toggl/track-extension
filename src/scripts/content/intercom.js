f6cd19bf952a9fbd659e4b81755d34d3ec4a4da2
Maciej Rylko <rylko.maciej@gmail.com>
2021-06-29 11:46:45 +0200

    fix(intercom): Handle Intercom in 2021

--- src/scripts/content/intercom.js
+++ src/scripts/content/intercom.js
@@ -1,51 +1,18 @@
 'use strict';
 
-togglbutton.render('.conversation__card__content-expanded__controls .inbox__conversation-controls__pane-selector:not(.toggl)',
-  { observe: true },
-  function (elem) {
-    if (elem.querySelector('.toggl-button')) {
-      // With the way this UI renders, we must check for existence of the button.
-      return;
+// Intercom 2021
+togglbutton.render('[data-conversation-id] > * [data-intercom-target="conversation-card-header"]:not(.toggl)', { observe: true }, elem => {
+  const descriptionSelector = () => {
+    const titleContainer = elem.querySelector('.conversation__card__title__text');
+    const customTitleInput = titleContainer.querySelector('input');
+
+    if (customTitleInput && customTitleInput.value) {
+      return customTitleInput.value;
     }
 
-    const root = elem.closest('.card.conversation__card');
-    const descriptionSelector = () => {
-      const description = $('.inbox__card__header__title', root);
-      return description ? description.textContent.trim().replace(/ +/g, ' ') : '';
-    };
+    return titleContainer.textContent.replace(/\s\s+/g, ' ').replaceAll('\n', '').trim();
+  };
+  const link = togglbutton.createTimerLink({ className: 'intercom', description: descriptionSelector, buttonType: 'minimal' });
 
-    /**
-     * @type {HTMLElement}
-     */
-    const link = togglbutton.createTimerLink({
-      className: 'intercom',
-      description: descriptionSelector
-    });
-    link.style.paddingRight = '15px';
-
-    // Stop button being re-rendered while trying to click. Intercom has some kind of handlers doing things.
-    link.addEventListener('mousedown', (e) => {
-      e.preventDefault();
-      e.stopPropagation();
-    });
-
-    elem.appendChild(link);
-  });
-
-togglbutton.render('.articles__editor__header-text:not(.toggl)',
-  { observe: true },
-  function (elem) {
-    const descriptionSelector = () => {
-      const description = elem.textContent;
-      return description ? description.trim() : '';
-    };
-
-    const link = togglbutton.createTimerLink({
-      className: 'intercom',
-      description: descriptionSelector,
-      buttonType: 'minimal'
-    });
-    link.style.margin = '3px 15px';
-
-    elem.appendChild(link);
-  });
+  elem.appendChild(link);
+});

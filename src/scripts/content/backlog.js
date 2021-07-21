7ece29d245404033da32b13f2831867f1a46db1d
tomcat0090 <merge.abort.conflict@gmail.com>
2021-07-21 13:35:29 +0900

    add issue-table and view header

--- src/scripts/content/backlog.js
+++ src/scripts/content/backlog.js
@@ -1,23 +1,59 @@
 'use strict';
 
-togglbutton.render('#issueArea:not(.toggl)', { observe: true }, function (elem) {
-  const container = createTag('span', '');
-  const ticketNumElem = $('.ticket__key .ticket__key-number', elem);
-  const titleElem = $('#summary .title-group__title-text', elem);
-  const projectElem = $('.project-header .header-icon-set__name');
-  const containerElem = $('#summary *:first-child');
+// Backlog Q1 2019: Task view
+togglbutton.render(
+  '#issueArea:not(.toggl)',
+  { observe: true },
+  elem => {
+    const descriptionText = () => `${elem.querySelector('.ticket__key .ticket__key-number').textContent} ${elem.querySelector('#summary .title-group__title-text').textContent}`;
+    const projectText = () => document.querySelector('.project-header .header-icon-set__name').textContent;
 
-  const descFunc = function () {
-    return ticketNumElem.textContent + ' ' + titleElem.textContent;
-  };
+    const link = togglbutton.createTimerLink({
+      className: 'Backlog',
+      description: descriptionText,
+      projectName: projectText,
+      calculateTotal: true
+    });
 
-  const link = togglbutton.createTimerLink({
-    className: 'Backlog',
-    description: descFunc,
-    projectName: projectElem.textContent,
-    calculateTotal: true
-  });
+    elem.querySelector('#summary *:first-child').appendChild(link);
+  }
+);
 
-  container.appendChild(link);
-  containerElem.parentNode.appendChild(container, containerElem);
-});
+// Backlog Q3 2021: Task header
+togglbutton.render(
+  '#ui-state-disabledissue:not(.toggl)',
+  { observe: true },
+  elem => {
+    const descriptionText = () => `${elem.querySelector('.header-icon-set__key').textContent} ${elem.querySelector('.header-icon-set__summary').textContent}`;
+    const projectText = () => document.querySelector('.project-header .header-icon-set__name').textContent;
+
+    const link = togglbutton.createTimerLink({
+      buttonType: 'minimal',
+      className: 'Backlog',
+      description: descriptionText,
+      projectName: projectText,
+      calculateTotal: true
+    });
+
+    elem.querySelector('.header-icon-set__text.content-header__summary').after(link);
+  }
+);
+
+// Backlog Q3 2021: Issues table
+togglbutton.render(
+  '#issues-table tbody tr:not(.toggl)',
+  { observe: true },
+  elem => {
+    const descriptionText = () => `${elem.querySelector('.cell-key').textContent} ${elem.querySelector('.cell-summary').textContent}`;
+    const projectText = () => (elem.querySelector('.cell-project-name') || document.querySelector('.project-header .header-icon-set__name')).textContent;
+
+    const link = togglbutton.createTimerLink({
+      buttonType: 'minimal',
+      className: 'Backlog-2021',
+      description: descriptionText,
+      projectName: projectText
+    });
+
+    elem.querySelector('td:nth-child(2)').prepend(link);
+  }
+);

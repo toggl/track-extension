e0f14f51d510e8f4e8ce568d885a2736e8960021
Maciej Rylko <rylko.maciej@gmail.com>
2021-08-02 09:52:40 +0200

    fix(zendesk): Restore previous Zendesk integrations
    Closes #1992

--- src/scripts/content/zendesk.js
+++ src/scripts/content/zendesk.js
@@ -1,23 +1,95 @@
 'use strict';
 
 // Zendesk new UI Jul 2021
-togglbutton.render('.omni-conversation-pane [class^=styles__Header]:not(.toggl)', { observe: true }, elem => {
-  const getProject = () => {
-    const title = document.querySelector('title');
-    return title ? title.textContent.trim() : '';
-  };
+togglbutton.render(
+  '.omni-conversation-pane [class^=styles__Header]:not(.toggl)',
+  { observe: true },
+  (elem) => {
+    const getProject = () => {
+      const title = document.querySelector('title');
+      return title ? title.textContent.trim() : '';
+    };
 
-  const getDescription = () => {
-    const input = elem.querySelector('[class^=styles__Left] input');
-    return (input ? input.value : '').trim();
-  };
+    const getDescription = () => {
+      const input = elem.querySelector('[class^=styles__Left] input');
+      return (input ? input.value : '').trim();
+    };
 
-  const link = togglbutton.createTimerLink({
-    buttonType: 'minimal',
-    className: 'zendesk',
-    description: getDescription,
-    projectName: getProject
-  });
+    const link = togglbutton.createTimerLink({
+      buttonType: 'minimal',
+      className: 'zendesk--2021',
+      description: getDescription,
+      projectName: getProject
+    });
 
-  elem.insertBefore(link, elem.querySelector('[class^=styles__Right]'));
-});
+    elem.insertBefore(link, elem.querySelector('[class^=styles__Right]'));
+  }
+);
+
+// Zendesk pre-2021
+togglbutton.render(
+  '.pane_header:not(.toggl)',
+  { observe: true },
+  function (elem) {
+    let description;
+    const projectName = $('title').textContent;
+
+    const titleFunc = function () {
+      const titleElem = $('.editable .ember-view input', elem);
+      const ticketNum = location.href.match(/tickets\/(\d+)/);
+
+      if (titleElem !== null) {
+        description = titleElem.value.trim();
+      }
+
+      if (ticketNum) {
+        description = '#' + ticketNum[1].trim() + ' ' + description;
+      }
+      return description;
+    };
+
+    const link = togglbutton.createTimerLink({
+      className: 'zendesk',
+      description: titleFunc,
+      projectName: projectName && projectName.split(' - ').shift()
+    });
+
+    // Check for strange duplicate buttons. Don't know why this happens in Zendesk.
+    if (elem.querySelector('.toggl-button')) {
+      elem.removeChild(elem.querySelector('.toggl-button'));
+    }
+
+    elem.insertBefore(link, elem.querySelector('.btn-group'));
+  }
+);
+
+const getDescription = () => {
+  const ticketNum = location.href.match(/tickets\/(\d+)/);
+
+  if (!ticketNum) return null;
+  const id = ticketNum[1].trim();
+  const titleElem = document.querySelector(
+    `[data-side-conversations-anchor-id="${id}"] input[aria-label="Subject"]`
+  );
+  if (!titleElem) return null;
+
+  return '#' + id + ' ' + titleElem.value.trim();
+};
+
+togglbutton.render(
+  '[data-test-id="customer-context-tab-navigation"]',
+  { observe: true },
+  function (elem) {
+    // Manual check for existence in this SPA.
+    if (elem.querySelector('.toggl-button')) return;
+    // If we can't get the description on this pass, let's skip and wait for the next one
+    if (!getDescription()) return;
+
+    const link = togglbutton.createTimerLink({
+      className: 'zendesk-agent-ws',
+      description: getDescription
+    });
+
+    elem.prepend(link);
+  }
+);

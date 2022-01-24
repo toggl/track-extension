4ccddef8c8dbaef63a2ac4ab573e5d16528b2db6
Andrey Grunev <s.eternallight@gmail.com>
2022-01-24 13:57:33 +0200

    fix(taiga): Fix taiga.io integration

--- src/content/taiga.js
+++ src/content/taiga.js
@@ -10,7 +10,7 @@ togglbutton.render(
   '.detail-title-wrapper:not(.toggl)',
   { observe: true },
   function (elem) {
-    const projectElem = $('tg-legacy-loader').shadowRoot.querySelector('.menu-option-text.project-name');
+    const projectElem = $('tg-project-menu tg-legacy-loader').shadowRoot.querySelector('.menu-option-text.project-name');
     const refElem = $('.detail-ref', elem);
     const titleElem = $('.detail-title-text > span', elem);
 
@@ -43,12 +43,12 @@ togglbutton.render('.epic-row .name:not(.toggl)', { observe: true }, function (
 });
 
 /* Backlog buttons */
-togglbutton.render('.user-story-name:not(.toggl)', { observe: true }, function (
+togglbutton.render('.user-story-link:not(.toggl)', { observe: true }, function (
   elem
 ) {
-  const projectElem = $('.backlog .project-name');
-  const refElem = $('a > span:nth-child(1)', elem);
-  const taskElem = $('a > span:nth-child(2)', elem);
+  const projectElem = $('tg-project-menu tg-legacy-loader').shadowRoot.querySelector('.menu-option-text.project-name');
+  const refElem = $('.user-story-number', elem);
+  const taskElem = $('.user-story-name', elem);
 
   const link = togglbutton.createTimerLink({
     className: 'taiga',
@@ -57,7 +57,7 @@ togglbutton.render('.user-story-name:not(.toggl)', { observe: true }, function (
     description: refElem.textContent.trim() + ' ' + taskElem.textContent
   });
 
-  elem.insertBefore(link, $('a', elem));
+  elem.prepend(link);
 });
 
 /* Kanban buttons */
@@ -65,9 +65,9 @@ togglbutton.render(
   '.kanban .card-title:not(.toggl)',
   { observe: true },
   function (elem) {
-    const refElem = $('a > span:nth-child(1)', elem);
-    const titleElem = $('a > span:nth-child(2)', elem);
-    const projectElem = $('.kanban .project-name');
+    const refElem = $('a > .card-ref', elem);
+    const titleElem = $('a > .card-subject', elem);
+    const projectElem = $('tg-project-menu tg-legacy-loader').shadowRoot.querySelector('.menu-option-text.project-name');
 
     const link = togglbutton.createTimerLink({
       className: 'taiga',
@@ -105,9 +105,9 @@ togglbutton.render(
   '.issues-table .row:not(.toggl)',
   { observe: true },
   function (elem) {
-    const projectElem = $('.issues-page .project-name');
-    const refElem = $('a > span:nth-child(1)', elem);
-    const taskElem = $('a > span:nth-child(2)', elem);
+    const projectElem = $('tg-project-menu tg-legacy-loader').shadowRoot.querySelector('.menu-option-text.project-name');
+    const refElem = $('.subject .issue-ref', elem);
+    const taskElem = $('.subject .issue-subject', elem);
 
     const link = togglbutton.createTimerLink({
       className: 'taiga',

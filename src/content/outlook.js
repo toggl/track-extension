01d153934fbb49b9c471bda50cb207771c381167
Nuno Nogueira <nunofmn@gmail.com>
2022-02-07 17:51:56 +0000

    fix(outlook): Fix Outlook integration
    Closes #2045

--- src/content/outlook.js
+++ src/content/outlook.js
@@ -1,27 +1,36 @@
-'use strict';
+"use strict";
 
 // Inbox emails
-togglbutton.render('[aria-label="Content pane"] [role="heading"]:not(.toggl)', { observe: true }, elem => {
-  const link = togglbutton.createTimerLink({
-    className: 'outlook',
-    description: elem.textContent
-  });
-
-  elem.appendChild(link);
-});
-
-// Composing emails
-togglbutton.render('[aria-label="Command toolbar"] .ms-CommandBar-primaryCommand:not(.toggl)', { observe: true }, elem => {
-  const isComposingEmail = elem.querySelector('button[name="Send"]');
-
-  if (isComposingEmail) {
-    const subject = () => document.querySelector('[aria-label="Add a subject"]').value;
-
+togglbutton.render(
+  '[aria-label="Message actions"]:not(.toggl)',
+  { observe: true },
+  (elem) => {
     const link = togglbutton.createTimerLink({
-      className: 'outlook',
-      description: subject
+      className: "outlook",
+      description: elem.textContent,
     });
 
-    elem.appendChild(link);
+    elem.childNodes[0].appendChild(link);
   }
-});
+);
+
+// Composing emails
+togglbutton.render(
+  '[aria-label="Command toolbar"] .ms-CommandBar-primaryCommand:not(.toggl)',
+  { observe: true },
+  (elem) => {
+    const isComposingEmail = elem.querySelector('button[name="Send"]');
+
+    if (isComposingEmail) {
+      const subject = () =>
+        document.querySelector('[aria-label="Add a subject"]').value;
+
+      const link = togglbutton.createTimerLink({
+        className: "outlook",
+        description: subject,
+      });
+
+      elem.appendChild(link);
+    }
+  }
+);

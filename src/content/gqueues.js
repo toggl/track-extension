bc481dfa1d779baa247197abd569bf1016ff6290
Daniel Paulino <danielpaulino030@gmail.com>
2022-06-20 03:41:42 -0300

    fix(gqueues): Fix GQueues integration
    close #1869

--- src/content/gqueues.js
+++ src/content/gqueues.js
@@ -1,24 +1,33 @@
-'use strict';
+/**
+ * @name GQueues
+ * @urlAlias gqueues.com
+ * @urlRegex *://*.gqueues.com/*
+ */
+ 'use strict';
 
 function insertAfter (newNode, referenceNode) {
   referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
 
 togglbutton.render(
-  '#gqItemList .gq-item-row:not(.toggl)',
+  '#gqItemList .gq-task-row:not(.toggl)',
   { observe: true },
   function (elem) {
     let link;
     const container = createTag('div', 'taskItem-toggl');
-    const titleElem = $('.gq-i-description', elem);
-    const projectContainer = $('.gq-queue-container.selected .gq-queue-name');
+    const titleElem = $('.gq-task-title', elem);
+    const projectContainer = $('#gqQueueContentHeader #gqQueueTitle input') || {};
 
     if (titleElem) {
       link = togglbutton.createTimerLink({
         className: 'gqueues',
         buttonType: 'minimal',
-        description: titleElem.textContent,
-        projectName: projectContainer.textContent
+        description: () => {
+          return titleElem.textContent
+        },
+        projectName: () => {
+          return projectContainer.value
+        }
       });
 
       container.appendChild(link);

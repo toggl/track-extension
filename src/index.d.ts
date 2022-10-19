18e7f892c3fd19eb6c99ead75770a985cc4177a8
Alisson Oliveira Souza <alisson_tecsouza@hotmail.com>
2022-10-19 12:30:54 -0300

    feat(types): Adds type definitions
    Closes toggl/track-extension-core#294

---
+++ src/index.d.ts
@@ -0,0 +1,11 @@
+import origins from './origins';
+
+declare const _default = origins as {
+  [key: string]: {
+    name: string;
+    url: string;
+    file?: string;
+  };
+};
+
+export default _default;

0f0e92c28669098d23c3d049bc6d4ad432932338
Pavel Kvach <pavel.kvach@gmail.com>
2024-03-05 07:42:11 +0200

    fix(youtrack): Fix Toggl button disappeared after UI update (#2270)
    * fix(youtrack): Fix Toggl button disappeared after UI update
    
    Closes #2265
    
    * feat(youtrack): Update domain and integration for issues
    
    ---------
    
    Co-authored-by: Shrey Gupta <connectwithshrey@gmail.com>

--- src/content/youtrack.js
+++ src/content/youtrack.js
@@ -44,6 +44,34 @@ togglbutton.render(
   }
 );
 
+/* new view for single issues — since YouTrack 2023.3 */
+togglbutton.render(
+  'div[data-test="issue-container"]:not(.toggl)',
+  { observe: true },
+  function (elem) {
+    console.log('Toggl Button: Reporter info not found.')
+    const reporterInfo = elem.querySelector('span[data-test="reporter-info"]');
+    if (reporterInfo === null) {
+      console.log('Toggl Button: Reporter info not found.')
+      return;
+    }
+    const reporterInfoContainer = reporterInfo.parentElement;
+
+    const issueIdElem = reporterInfoContainer.querySelector('a[href*="issue/"] > span');
+    const issueId = issueIdElem ? issueIdElem.textContent.trim() : "";
+
+    const issueTitleElem = elem.querySelector('h1');
+    const issueTitle = issueTitleElem ? issueTitleElem.textContent.trim() : "";
+
+    const link = togglbutton.createTimerLink({
+      description: issueId + ' ' + issueTitle,
+      projectName: issueId.split('-')[0]
+    });
+
+    reporterInfoContainer.insertBefore(link, reporterInfo);
+  }
+);
+
 // Agile board
 togglbutton.render('.yt-agile-card:not(.toggl)', { observe: true }, function (
   elem

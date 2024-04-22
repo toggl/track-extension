83168b076bb99b67260790db0060472870cf4df3
Shrey Gupta <connectwithshrey@gmail.com>
2024-04-22 12:34:17 +0530

    fix(clickup): Switch subtasks correctly

--- src/content/clickup.js
+++ src/content/clickup.js
@@ -1,18 +1,30 @@
+/**
+ * @name ClickUp
+ * @urlAlias clickup.com
+ * @urlRegex *://*.clickup.com/*
+ */
 'use strict';
+
+function getDescription() {
+  const description =  document.querySelector('title').textContent.split('|')[0];
+  const taskID = document.querySelector('div[role="dialog"]').getAttribute('data-task-id');
+
+  return `${description} - #${taskID}`;
+}
 
 togglbutton.render('#timeTrackingItem:not(.toggl)', { observe: true }, function (
   elem
 ) {
 
-  const description = document.querySelector('title').textContent.split('|')[0];
-  const taskID = document.querySelector('div[role="dialog"]').getAttribute('data-task-id');
-
-  const project = elem.querySelector('.space-name').textContent;
+  function getProject() {
+    const project = elem.querySelector('.space-name').textContent;
+    return project
+  }
 
   const link = togglbutton.createTimerLink({
     className: 'clickup',
-    description: `${description} - #${taskID}`,
-    projectName: project,
+    description: getDescription,
+    projectName: getProject,
     buttonType: 'minimal'
   });
 

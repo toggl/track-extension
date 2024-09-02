529676edf2d363dc32d3063e3657826891494bc4
Renato Pozzi <askides@proton.me>
2024-09-02 13:09:31 +0200

    fix(wrike): selector broken (#2325)
    * chore(wrike): lint code
    
    * fix(wrike): project selector

--- src/content/wrike.js
+++ src/content/wrike.js
@@ -4,43 +4,42 @@
  * @urlRegex *://*.wrike.com/*
  */
 
-'use strict';
-
+'use strict'
 
 togglbutton.render(
   'work-item-title:not(.toggl)',
   { observe: true },
   function (elem) {
     const container = document.querySelector('action-panel')
-    const viewContainer = document.querySelector('entity-view')
+    const viewContainer = document.querySelector('work-item-location')
 
     const getTitleElement = function () {
-      const wsTaskTitle = elem.querySelectorAll('task-title');
+      const wsTaskTitle = elem.querySelectorAll('task-title')
       if (wsTaskTitle.length === 1 && wsTaskTitle[0].textContent !== '') {
-        return wsTaskTitle[0];
+        return wsTaskTitle[0]
       }
-      return $('title');
-    };
+      return $('title')
+    }
 
     const descriptionText = function () {
-      const titleElem = getTitleElement();
-      const titleElemText = titleElem ? titleElem.textContent : 'not found';
-    
-      return `${titleElemText.trim().replace(' - Wrike', '')}`.trim();
-    };
+      const titleElem = getTitleElement()
+      const titleElemText = titleElem ? titleElem.textContent : 'not found'
+
+      return `${titleElemText.trim().replace(' - Wrike', '')}`.trim()
+    }
 
     const projectText = function () {
-      const projectElem = viewContainer.querySelector('wrike-tag');
+      const projectElem = viewContainer.querySelector('wrike-tag')
       // We process the project element text content.
-      return projectElem ? projectElem.textContent : '';
-    };
+      return projectElem ? projectElem.textContent : ''
+    }
 
     const link = togglbutton.createTimerLink({
       className: 'wrike',
       description: descriptionText,
-      projectName: projectText
-    });
+      projectName: projectText,
+    })
 
-    container.prepend(link);
-  }
-);
+    container.prepend(link)
+  },
+)

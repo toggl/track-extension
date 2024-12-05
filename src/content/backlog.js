84df83976ac070306eef3ad9e01ad63fad95306e
Renato Pozzi <askides@proton.me>
2024-12-05 10:47:45 +0100

    fix(backlog): add new origin (#2359)
    * chore(backlog): apply formatting
    
    * fix(backlog): add new origin
    
    * Update origins.js

--- src/content/backlog.js
+++ src/content/backlog.js
@@ -1,23 +1,33 @@
-'use strict';
+/**
+ * @name Backlog
+ * @urlAlias backlog.com
+ * @urlRegex *://*.backlog.com/*
+ */
 
-togglbutton.render('#issueArea:not(.toggl)', { observe: true }, function (elem) {
-  const container = createTag('span', '');
-  const ticketNumElem = $('.ticket__key .ticket__key-number', elem);
-  const titleElem = $('#summary .title-group__title-text', elem);
-  const projectElem = $('.project-header .header-icon-set__name');
-  const containerElem = $('#summary *:first-child');
+'use strict'
 
-  const descFunc = function () {
-    return ticketNumElem.textContent + ' ' + titleElem.textContent;
-  };
+togglbutton.render(
+  '#issueArea:not(.toggl)',
+  { observe: true },
+  function (elem) {
+    const container = createTag('span', '')
+    const ticketNumElem = $('.ticket__key .ticket__key-number', elem)
+    const titleElem = $('#summary .title-group__title-text', elem)
+    const projectElem = $('.project-header .header-icon-set__name')
+    const containerElem = $('#summary *:first-child')
 
-  const link = togglbutton.createTimerLink({
-    className: 'Backlog',
-    description: descFunc,
-    projectName: projectElem.textContent,
-    calculateTotal: true
-  });
+    const descFunc = function () {
+      return ticketNumElem.textContent + ' ' + titleElem.textContent
+    }
 
-  container.appendChild(link);
-  containerElem.parentNode.appendChild(container, containerElem);
-});
+    const link = togglbutton.createTimerLink({
+      className: 'Backlog',
+      description: descFunc,
+      projectName: projectElem.textContent,
+      calculateTotal: true,
+    })
+
+    container.appendChild(link)
+    containerElem.parentNode.appendChild(container, containerElem)
+  },
+)

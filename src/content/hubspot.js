e46c3b16fd49bd4ac91586fc5266273037240f9a
Sanjin Kapetanovic <167187877+sanjinkapetanovich@users.noreply.github.com>
2026-01-15 18:02:58 +0100

    fix(hubspot): integration buton issue (#2404)

--- src/content/hubspot.js
+++ src/content/hubspot.js
@@ -3,56 +3,62 @@
  * @urlAlias hubspot.com
  * @urlRegex *://app.hubspot.com/*
  */
-'use strict';
+'use strict'
 
 // This generic implementation works for most objects details page
 togglbutton.render(
-  'div[data-selenium-component="ProfileHighlightContainer"] [data-selenium-test="highlightTitle"]:not(.toggl)',
+  '[data-card-type="OBJECT_HIGHLIGHT"]:not(.toggl)',
   { observe: true },
-  $container => {
+  ($container) => {
     try {
       function descriptionSelector() {
-        const $description = $('div[data-selenium-test$="-highlight-details"] h3') ?? $('[data-selenium-test="highlightTitle"]');
-        return $description.textContent.trim();
+        const $description =
+          $('div[data-selenium-test$="-highlight-details"] h3') ??
+          $('[data-selenium-test="highlightTitle"]')
+        return $description.textContent.trim()
       }
 
       const link = togglbutton.createTimerLink({
         className: 'hubspot',
         description: descriptionSelector,
         autoTrackable: true,
-      });
-      const rowContainer = document.createElement('div');
-      rowContainer.setAttribute('class', 'flex-row align-center');
-      rowContainer.appendChild(link);
+      })
+      const rowContainer = document.createElement('div')
+      rowContainer.setAttribute('class', 'flex-row align-center')
+      rowContainer.appendChild(link)
       const cardContainer = $('[data-selenium-test="highlightTitle"]')
-      cardContainer.parentNode.parentNode.appendChild(rowContainer);
+      const parent = cardContainer.parentNode.parentNode.parentNode
+      parent.insertAdjacentElement('afterend', rowContainer)
     } catch (e) {
       console.error(e)
     }
-  }
-);
+  },
+)
 
 // This generic implementation works for most objects details page
 togglbutton.render(
   'td[data-table-external-id*="name-"]:not(.toggl)',
   { observe: true },
-  $container => {
+  ($container) => {
     try {
       function descriptionSelector() {
-        return $('[data-test-id="truncated-object-label"]', $container).textContent.trim();
+        return $(
+          '[data-test-id="truncated-object-label"]',
+          $container,
+        ).textContent.trim()
       }
 
       const link = togglbutton.createTimerLink({
         className: 'hubspot-list-item',
         description: descriptionSelector,
-        buttonType: 'minimal'
-      });
-      const rowContainer = document.createElement('div');
-      rowContainer.setAttribute('class', 'flex-row align-center');
-      rowContainer.appendChild(link);
-      $('.media', $container).prepend(rowContainer);
+        buttonType: 'minimal',
+      })
+      const rowContainer = document.createElement('div')
+      rowContainer.setAttribute('class', 'flex-row align-center')
+      rowContainer.appendChild(link)
+      $('.media', $container).prepend(rowContainer)
     } catch (e) {
       console.error(e)
     }
-  }
-);
+  },
+)
